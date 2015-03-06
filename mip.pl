08fbd414e1e5f284015b954243ce148177e151a4
henrikstranneheim <henrik.stranneheim@scilifelab.se>
2015-03-06 10:34:21 +0100

    Bumped version

--- mip.pl
+++ mip.pl
@@ -528,7 +528,7 @@ my (@GATKTargetPaddedBedIntervalLists);  #Array for target infile lists used in 
 ##humanGenomeReference
 &DefineParametersPath(\%parameter, \@orderParameters, "humanGenomeReference", "Homo_sapiens.GRCh37.d5.fasta", "pBwaMem,pBwaAln,pBwaSampe,pGATKRealigner,pGATKBaseRecalibration,pGATKHaploTypeCaller,pGATKGenoTypeGVCFs,pGATKVariantRecalibration,pGATKPhaseByTransmission,pGATKReadBackedPhasing,pGATKVariantEvalAll,pGATKVariantEvalExome,pAnnovar,pPicardToolsCalculateHSMetrics,pPicardToolsCollectMultipleMetrics", "file", "yesAutoDownLoad");
 
-my $mipVersion = "v2.2.1";  #Set version
+my $mipVersion = "v2.2.2";  #Set version
 
 my ($aligner) = ("nocmdinput");
 
