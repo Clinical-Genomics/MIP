30407c17d508c36720c66ab84cf4211771b4bd67
henrikstranneheim <henrik.stranneheim@scilifelab.se>
2015-11-02 13:25:48 +0100

    Added more options to install.pl
    - Conda path
    - Check for binaries
    - update

--- install.pl
+++ install.pl
@@ -15,6 +15,7 @@ BEGIN {
     $USAGE =
 	qq{install.pl [options]
            -env/--condaEnvironment Conda environment (Default: "mip")
+           -cdp/--condaPath The conda path (Default: "HOME/miniconda")
            -bvc/--bioConda Set the module version of the programs that can be installed with bioConda (i.e. 'bwa=0.7.12')
            -pm/perlModules Set the perl modules to be installed via cpanm (comma sep)
            -pip/--pip Set the module version of the programs that can be installed with pip (i.e. 'genmod=3.3.3')
@@ -25,6 +26,7 @@ BEGIN {
            -vep/--VariantEffectPredictor Set the VEP version (Default: "82")
 	   -vepc/--vepDirectoryCache Specify the cache directory to use (whole path; defaults to "~/miniconda/envs/condaEnvironment/ensembl-tools-release-VariantEffectPredictorVersion/cache")
            -ppd/--printParameterDefaults Print the parameter defaults
+           -u/--update Always install program
            -h/--help Display this help message   
            -v/--version Display version
         };    
@@ -36,7 +38,9 @@ my %parameter;
 ### Set parameter default
 
 ##Conda
+$parameter{'update'} = 1;
 $parameter{'condaEnvironment'} = "mip";
+$parameter{'condaPath'} = $ENV{HOME}."/miniconda";
 $parameter{'bioConda'}{'bwa'} = "0.7.12";
 $parameter{'bioConda'}{'fastqc'} = "0.11.2";
 $parameter{'bioConda'}{'samtools'} = "1.2";
@@ -77,12 +81,13 @@ $parameter{'vcfTools'} = "0.1.14";
 $parameter{'vt'} = "gitRepo";
 $parameter{'plink'} = "1.07";
 $parameter{'VariantEffectPredictor'} = "82";
-$parameter{'vepDirectoryCache'} = q?~/miniconda/envs/?.$parameter{'condaEnvironment'}.q?/ensembl-tools-release-?.$parameter{'VariantEffectPredictor'}.q?/cache?;  #Cache directory;
+$parameter{'vepDirectoryCache'} = $parameter{'condaPath'}.q?/envs/?.$parameter{'condaEnvironment'}.q?/ensembl-tools-release-?.$parameter{'VariantEffectPredictor'}.q?/cache?;  #Cache directory;
 
 my $installVersion = "0.0.1";
 
 ###User Options
 GetOptions('env|condaEnvironment:s'  => \$parameter{'condaEnvironment'},
+	   'cdp|condaPath:s' => \$parameter{'condaPath'},
 	   'bcv|bioConda=s'  => \%{$parameter{'bioConda'}},
 	   'pm|perlModules:s'  => \@{$parameter{'perlModules'}},  #Comma separated list
 	   'pip|pip=s'  => \%{$parameter{'pip'}},
@@ -93,6 +98,7 @@ GetOptions('env|condaEnvironment:s'  => \$parameter{'condaEnvironment'},
 	   'vep|VariantEffectPredictor:s'  => \$parameter{'VariantEffectPredictor'},
 	   'vepc|vepDirectoryCache:s'  => \$parameter{'vepDirectoryCache'},  #path to vep cache dir
 	   'ppd|printParameterDefaults'  => sub { &PrintParameters(\%parameter); exit;},  #Display parameter defaults
+	   'u|update:n' => \$parameter{'update'},
 	   'h|help' => sub { print STDOUT $USAGE, "\n"; exit;},  #Display help text
 	   'v|version' => sub { print STDOUT "\ninstall.pl ".$installVersion, "\n\n"; exit;},  #Display version number
     );
@@ -106,7 +112,7 @@ my $BASHFILEHANDLE = &CreateBashFile("mip.sh");
 
 &CreateCondaEnvironment(\%parameter, $BASHFILEHANDLE);
 
-&InstallCpanmAndModules(\%parameter, $BASHFILEHANDLE);
+&Perl(\%parameter, $BASHFILEHANDLE);
 
 &PipInstall(\%parameter, $BASHFILEHANDLE);
 
@@ -180,6 +186,15 @@ sub CreateCondaEnvironment {
     my $parameterHashRef = $_[0];
     my $FILEHANDLE = $_[1];
 
+    ## Check Conda path
+    if (! -d $parameter{'condaPath'}) {
+
+	print STDERR "Could not find miniconda directory in: ".$parameter{'condaPath'}, "\n";
+	exit 1;
+    }
+
+    print STDERR "Writting install instructions for Conda packages", "\n";
+
     ## Update Conda
     print $FILEHANDLE "### Update Conda\n";
     print $FILEHANDLE "conda update -y conda ";
@@ -201,91 +216,125 @@ sub CreateCondaEnvironment {
 }
 
 
-sub InstallCpanmAndModules {
+sub Perl {
+    
+    my $parameterHashRef = $_[0];
+    my $FILEHANDLE = $_[1];
+    
+    my $pwd = cwd();
+    
+    if ($ENV{PATH}=~/perl-${$parameterHashRef}{'perl'}/) {
+	
+	if (${$parameterHashRef}{'update'} == 0) {
+	    
+	    print STDERR "Found perl-".${$parameterHashRef}{'perl'}.". in your path\n";
+	    print STDERR q?Skipping writting installation for perl-?.${$parameterHashRef}{'perl'},"\n";  
+	}
+	else {
+	    
+	    ## Removing specific Perl version
+	    print $FILEHANDLE "### Removing specific Perl version\n";
+	    print $FILEHANDLE q?rm -rf $HOME/perl-?.${$parameterHashRef}{'perl'};
+	    print $FILEHANDLE "\n\n";
+	    
+	    &InstallPerlCpnam($parameterHashRef, $BASHFILEHANDLE); 
+	}
+    }
+    else {
+    	
+	&InstallPerlCpnam($parameterHashRef, $BASHFILEHANDLE, "AddPath");
+
+	## Install Perl modules via cpanm
+	print $FILEHANDLE "## Install Perl modules via cpanm\n";
+	print $FILEHANDLE "cpanm ";
+	print $FILEHANDLE join(" ", @{${$parameterHashRef}{'perlModules'}})." ";
+	print $FILEHANDLE "\n\n";
+    }
+}
+
+
+sub InstallPerlCpnam {
 
     my $parameterHashRef = $_[0];
     my $FILEHANDLE = $_[1];
-
-     my $pwd = cwd();
+    my $path = $_[2];
     
-    if ($ENV{PATH}=~/perl-${$parameterHashRef}{'perl'}/) {
+    my $pwd = cwd();
 
-	print STDOUT "Found perl-".${$parameterHashRef}{'perl'}.". in your path\n";
-    }
-    else {
-
-	## Install specific Perl version
-	print $FILEHANDLE "### Install specific Perl version\n";
-	
-	## Move to Home
-	print $FILEHANDLE "## Move HOME\n";
-	print $FILEHANDLE q?cd $HOME?;
-	print $FILEHANDLE "\n\n";
-	
-	## Download
-	print $FILEHANDLE "## Download Perl\n";
-	print $FILEHANDLE "wget --quiet http://www.cpan.org/src/5.0/perl-".${$parameterHashRef}{'perl'}.".tar.gz ";
-	print $FILEHANDLE "-O perl-".${$parameterHashRef}{'perl'}.".tar.gz";  #Dowload outfile
-	print $FILEHANDLE "\n\n";
-	
-	## Extract
-	print $FILEHANDLE "## Extract\n";
-	print $FILEHANDLE "tar xzf perl-".${$parameterHashRef}{'perl'}.".tar.gz";
-	print $FILEHANDLE "\n\n";
-	
-	## Move to perl directory
-	print $FILEHANDLE "## Move to perl directory\n";
-	print $FILEHANDLE "cd perl-".${$parameterHashRef}{'perl'};
-	print $FILEHANDLE "\n\n";
-	
-	## Configure
-	print $FILEHANDLE "## Configure\n";
-	print $FILEHANDLE q?./Configure -des -Dprefix=$HOME/perl-?.${$parameterHashRef}{'perl'};
-	print $FILEHANDLE "\n";
-	
-	print $FILEHANDLE "make";
-	print $FILEHANDLE "\n";
-	
-	print $FILEHANDLE "make test";
-	print $FILEHANDLE "\n";
-	
-	print $FILEHANDLE "make install";
-	print $FILEHANDLE "\n\n";
+    print STDERR "Writting install instructions for Perl and Cpanm", "\n";
+    
+    ## Install specific Perl version
+    print $FILEHANDLE "### Install specific Perl version\n";
+    
+    ## Move to Home
+    print $FILEHANDLE "## Move HOME\n";
+    print $FILEHANDLE q?cd $HOME?;
+    print $FILEHANDLE "\n\n";
+    
+    ## Download
+    print $FILEHANDLE "## Download Perl\n";
+    print $FILEHANDLE "wget --quiet http://www.cpan.org/src/5.0/perl-".${$parameterHashRef}{'perl'}.".tar.gz ";
+    print $FILEHANDLE "-O perl-".${$parameterHashRef}{'perl'}.".tar.gz";  #Dowload outfile
+    print $FILEHANDLE "\n\n";
+    
+    ## Extract
+    print $FILEHANDLE "## Extract\n";
+    print $FILEHANDLE "tar xzf perl-".${$parameterHashRef}{'perl'}.".tar.gz";
+    print $FILEHANDLE "\n\n";
+    
+    ## Move to perl directory
+    print $FILEHANDLE "## Move to perl directory\n";
+    print $FILEHANDLE "cd perl-".${$parameterHashRef}{'perl'};
+    print $FILEHANDLE "\n\n";
+    
+    ## Configure
+    print $FILEHANDLE "## Configure\n";
+    print $FILEHANDLE q?./Configure -des -Dprefix=$HOME/perl-?.${$parameterHashRef}{'perl'};
+    print $FILEHANDLE "\n";
+    
+    print $FILEHANDLE "make";
+    print $FILEHANDLE "\n";
+    
+    print $FILEHANDLE "make test";
+    print $FILEHANDLE "\n";
+    
+    print $FILEHANDLE "make install";
+    print $FILEHANDLE "\n\n";
+    
+    if ($path) {
 	
 	## Export path
 	print $FILEHANDLE "## Export path\n";
 	print $FILEHANDLE q?echo 'export PATH=$HOME/perl-?.${$parameterHashRef}{'perl'}.q?/:$PATH' >> ~/.bashrc?;
 	print $FILEHANDLE "\n\n";
-	
-	## Remove tar file
-	print $FILEHANDLE "## Remove tar file\n";
-	print $FILEHANDLE "cd && rm perl-".${$parameterHashRef}{'perl'}.".tar.gz";
-	print $FILEHANDLE "\n\n";
+    }
 
-	## Move to back
-	print $FILEHANDLE "## Move to original working directory\n";
-	print $FILEHANDLE "cd ".$pwd;
-	print $FILEHANDLE "\n\n";
+    ## Remove tar file
+    print $FILEHANDLE "## Remove tar file\n";
+    print $FILEHANDLE "cd && rm perl-".${$parameterHashRef}{'perl'}.".tar.gz";
+    print $FILEHANDLE "\n\n";
+    
+    ## Move to back
+    print $FILEHANDLE "## Move to original working directory\n";
+    print $FILEHANDLE "cd ".$pwd;
+    print $FILEHANDLE "\n\n";
 
-	## Use newly installed perl
-	print $FILEHANDLE q?eval `perl -I ~/perl-?.${$parameterHashRef}{'perl'}.q?/lib/perl5/ -Mlocal::lib=~/perl-?.${$parameterHashRef}{'perl'}.q?/` ?;
-	print $FILEHANDLE "\n";
+    if ($path) {
+
 	print $FILEHANDLE q?echo 'eval `perl -I ~/perl-?.${$parameterHashRef}{'perl'}.q?/lib/perl5/ -Mlocal::lib=~/perl-?.${$parameterHashRef}{'perl'}.q?/`' >> ~/.bash_profile ?;  #Add at start-up
 	print $FILEHANDLE "\n\n";
-
-	print $FILEHANDLE q?PERL5LIB=~/perl-?.${$parameterHashRef}{'perl'}.q?/lib/perl5?;
-	print $FILEHANDLE "\n\n";
-
-	## Install Perl modules via cpanm
-	print $FILEHANDLE "## Install cpanm\n";
-	print $FILEHANDLE q?wget -O- http://cpanmin.us | perl - -l $HOME/perl-?.${$parameterHashRef}{'perl'}.q?/bin App::cpanminus --local-lib=~/perl-?.${$parameterHashRef}{'perl'}.q?/ local::lib ?;
-	print $FILEHANDLE "\n\n";
     }
-    	
+    
     ## Install Perl modules via cpanm
-    print $FILEHANDLE "## Install Perl modules via cpanm\n";
-    print $FILEHANDLE "cpanm ";
-    print $FILEHANDLE join(" ", @{${$parameterHashRef}{'perlModules'}})." ";
+    print $FILEHANDLE "## Install cpanm\n";
+    print $FILEHANDLE q?wget -O- http://cpanmin.us | perl - -l $HOME/perl-?.${$parameterHashRef}{'perl'}.q?/bin App::cpanminus --local-lib=~/perl-?.${$parameterHashRef}{'perl'}.q?/ local::lib ?;
+    print $FILEHANDLE "\n\n";
+
+    ## Use newly installed perl
+    print $FILEHANDLE q?eval `perl -I ~/perl-?.${$parameterHashRef}{'perl'}.q?/lib/perl5/ -Mlocal::lib=~/perl-?.${$parameterHashRef}{'perl'}.q?/` ?;
+    print $FILEHANDLE "\n";
+
+    print $FILEHANDLE q?PERL5LIB=~/perl-?.${$parameterHashRef}{'perl'}.q?/lib/perl5?;
     print $FILEHANDLE "\n\n";
 }
     
@@ -294,6 +343,8 @@ sub PipInstall {
 
     my $parameterHashRef = $_[0];
     my $FILEHANDLE = $_[1];
+
+    print STDERR "Writting install instructions for pip packages", "\n";
 
     ## Install PIP packages in conda environment
     print $FILEHANDLE "### Install PIP packages in conda environment: ".${$parameterHashRef}{'condaEnvironment'}."\n";
@@ -321,6 +372,10 @@ sub Sambamba {
 
     my $pwd = cwd();
 
+    if (&CheckCondaBinFileExists($parameterHashRef, "sambamba", ${$parameterHashRef}{'sambamba'}) ) {
+
+	return 
+    }
     ## Install sambamba
     print $FILEHANDLE "### Install sambamba\n\n";
 
@@ -354,7 +409,7 @@ sub Sambamba {
     ## Make available from conda environment
     print $FILEHANDLE "## Make available from conda environment\n";
     print $FILEHANDLE "mv ";
-    print $FILEHANDLE q?sambamba_v?.${$parameterHashRef}{'sambamba'}.q? ~/miniconda/envs/?.${$parameterHashRef}{'condaEnvironment'}.q?/bin/?;
+    print $FILEHANDLE q?sambamba_v?.${$parameterHashRef}{'sambamba'}.q? ?.$parameter{'condaPath'}.q?/envs/?.${$parameterHashRef}{'condaEnvironment'}.q?/bin/?;
     print $FILEHANDLE "\n\n";
 
     &CleanUpModuleInstall($FILEHANDLE, $pwd);
@@ -367,6 +422,11 @@ sub VcfTools {
     my $FILEHANDLE = $_[1];
 
     my $pwd = cwd();
+
+    if(&CheckCondaBinFileExists($parameterHashRef, "vcftools")) {
+    
+	return
+    }
 
     ## Install vcfTools
     print $FILEHANDLE "### Install vcfTools\n\n";
@@ -395,7 +455,7 @@ sub VcfTools {
     print $FILEHANDLE "\n\n";
 
     ## Configure
-    my $filePath = glob(q?~/miniconda/envs/?.${$parameterHashRef}{'condaEnvironment'});
+    my $filePath = $parameter{'condaPath'}.q?/envs/?.${$parameterHashRef}{'condaEnvironment'};
 
     print $FILEHANDLE "## Configure\n";
     print $FILEHANDLE q?./configure --prefix=?.$filePath;
@@ -421,6 +481,11 @@ sub VT {
     my $FILEHANDLE = $_[1];
 
     my $pwd = cwd();
+
+    if (&CheckCondaBinFileExists($parameterHashRef, "vt")) {
+    
+	return
+    }
 
     ## Install sambamba
     print $FILEHANDLE "### Install VT\n\n";
@@ -449,7 +514,7 @@ sub VT {
     ## Make available from conda environment
     print $FILEHANDLE "## Make available from conda environment\n";
     print $FILEHANDLE "mv ";
-    print $FILEHANDLE q?vt ~/miniconda/envs/?.${$parameterHashRef}{'condaEnvironment'}.q?/bin/?;
+    print $FILEHANDLE q?vt ?.$parameter{'condaPath'}.q?/envs/?.${$parameterHashRef}{'condaEnvironment'}.q?/bin/?;
     print $FILEHANDLE "\n\n";
 
     &CleanUpModuleInstall($FILEHANDLE, $pwd);
@@ -462,6 +527,11 @@ sub Plink {
     my $FILEHANDLE = $_[1];
 
     my $pwd = cwd();
+
+    if (&CheckCondaBinFileExists($parameterHashRef, "plink")) {
+
+	return
+    }
 
     ## Install Plink
     print $FILEHANDLE "### Install Plink\n\n";
@@ -488,7 +558,7 @@ sub Plink {
     ## Make available from conda environment
     print $FILEHANDLE "## Make available from conda environment\n";
     print $FILEHANDLE "mv ";
-    print $FILEHANDLE q?plink ~/miniconda/envs/?.${$parameterHashRef}{'condaEnvironment'}.q?/bin/?;
+    print $FILEHANDLE q?plink ?.$parameter{'condaPath'}.q?/envs/?.${$parameterHashRef}{'condaEnvironment'}.q?/bin/?;
     print $FILEHANDLE "\n\n";
 
     &CleanUpModuleInstall($FILEHANDLE, $pwd);
@@ -501,6 +571,30 @@ sub VariantEffectPredictor {
     my $FILEHANDLE = $_[1];
 
     my $pwd = cwd();
+    
+    my $minicondaBinDirectory = $parameter{'condaPath'}.q?/envs/?.${$parameterHashRef}{'condaEnvironment'}.q?/ensembl-tools-release-?.${$parameterHashRef}{'VariantEffectPredictor'};
+
+    if (-d $minicondaBinDirectory) {
+
+	print STDERR q?Found VariantEffectPredictor in miniconda directory: ?.$minicondaBinDirectory, "\n";
+	
+	if (${$parameterHashRef}{'update'} == 0) {
+
+	    print STDERR "Skipping writting installation process for VariantEffectPredictor","\n";  	    
+	    return
+	}
+	else {
+
+	    ## Removing VariantEffectPredictor
+	    print $FILEHANDLE "### Removing VariantEffectPredictor\n";
+	    print $FILEHANDLE q?rm -rf ?.$minicondaBinDirectory;
+	    print $FILEHANDLE "\n\n";
+	}
+    }
+    else {
+	
+	print STDERR "Writting install instructions for VariantEffectPredictor", "\n";
+    }
 
     ## Install VEP
     print $FILEHANDLE "### Install VariantEffectPredictor\n\n";
@@ -512,7 +606,7 @@ sub VariantEffectPredictor {
     print $FILEHANDLE "\n\n";
 
     ## Move to miniconda environment
-    print $FILEHANDLE q?cd ~/miniconda/envs/?.${$parameterHashRef}{'condaEnvironment'};
+    print $FILEHANDLE q?cd ?.$parameter{'condaPath'}.q?/envs/?.${$parameterHashRef}{'condaEnvironment'};
     print $FILEHANDLE "\n\n";
 
     ## Download
@@ -542,7 +636,7 @@ sub VariantEffectPredictor {
 
     ## Clean up
     print $FILEHANDLE "## Clean up\n";
-    print $FILEHANDLE q?cd ~/miniconda/envs/?.${$parameterHashRef}{'condaEnvironment'};
+    print $FILEHANDLE q?cd ?.$parameter{'condaPath'}.q?/envs/?.${$parameterHashRef}{'condaEnvironment'};
     print $FILEHANDLE "\n\n";
 
     print $FILEHANDLE "rm -rf VariantEffectPredictor-".${$parameterHashRef}{'VariantEffectPredictor'}.".zip";;
@@ -601,4 +695,58 @@ sub CreateInstallDirectory {
     print $FILEHANDLE "\n";
     print $FILEHANDLE "cd .MIP";
     print $FILEHANDLE "\n\n";
+}
+
+
+sub CheckCondaBinFileExists {
+    
+    my $parameterHashRef = $_[0];
+    my $programName = $_[1];
+    my $programVersion = $_[2];
+    
+    my $minicondaBinFile = $parameter{'condaPath'};
+
+    if ($programName eq "sambamba") {
+	
+	$programVersion = "_v".${$parameterHashRef}{'sambamba'};
+    }
+    
+    if ($programVersion) {
+
+	$minicondaBinFile .= q?/envs/?.${$parameterHashRef}{'condaEnvironment'}.q?/bin/?.$programName.$programVersion;
+    }
+    else {
+
+	$minicondaBinFile .= q?/envs/?.${$parameterHashRef}{'condaEnvironment'}.q?/bin/?.$programName;
+    }
+
+    if (-f $minicondaBinFile) {
+
+	if ($programVersion) {
+	    
+	    print STDERR q?Found ?.$programName.q? version ?.$programVersion.q? in miniconda directory: ?.$parameter{'condaPath'}.q?/envs/?.${$parameterHashRef}{'condaEnvironment'}.q?/bin/?, "\n";
+	    
+	    if (${$parameterHashRef}{'update'} == 0) {
+
+		print STDERR q?Skipping writting installation process for ?.$programName.q? ?.$programVersion,"\n";  
+		return 1;
+	    }	
+	}   
+	else {
+	    
+	    print STDERR q?Found ?.$programName.q? in miniconda directory: ?.$parameter{'condaPath'}.q?/envs/?.${$parameterHashRef}{'condaEnvironment'}.q?/bin/?, "\n";
+	    
+	    if (${$parameterHashRef}{'update'} == 0) {
+		
+		print STDERR q?Skipping writting installation process for ?.$programName,"\n";  	    
+		return 1;
+	    }
+	}
+	return 0;
+    }
+    else {
+	
+	print STDERR "Writting install instructions for ".$programName, "\n";
+	return 0;
+    }
 }
