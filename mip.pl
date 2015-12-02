60297b772590f873ff51bf46fad5f267206edc0e
henrikstranneheim <henrik.stranneheim@scilifelab.se>
2015-12-02 11:00:37 +0100

    Bumped version

--- mip.pl
+++ mip.pl
@@ -273,7 +273,7 @@ chomp($dateTimeStamp, $date, $script);  #Remove \n;
 ## Eval parameter hash
 &EvalParameterHash(\%parameter, $Bin."/definitions/defineParameters.yaml");
 
-my $mipVersion = "v2.5.1";	#Set MIP version
+my $mipVersion = "v2.5.2";	#Set MIP version
 my $aligner;
 
 ## Target definition files
