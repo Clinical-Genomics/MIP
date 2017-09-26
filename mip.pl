baae123bdc2b92171f27282c4bdc98c02df62815
Chiara Rasi <rasi.chiara@gmail.com>
2017-09-26 15:53:09 +0200

    fixed bug

--- mip.pl
+++ mip.pl
@@ -1996,7 +1996,7 @@ if ( $active_parameter{pmanta} > 0 ) {    #Run Manta
             infile_lane_prefix_href => \%infile_lane_prefix,
             job_id_href             => \%job_id,
             program_name            => $program_name,
-            outfamily_directory     => $outfamily_directory.,
+            outfamily_directory     => $outfamily_directory,
         }
     );
 }
