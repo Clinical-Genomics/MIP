76ca17cc00e5cb03ca8130f8270b5d5e9bc37058
northwestwitch <rasi.chiara@gmail.com>
2017-12-05 13:43:43 +0100

    fixed a missing import

--- lib/MIP/Recipes/Analysis/Vt.pm
+++ lib/MIP/Recipes/Analysis/Vt.pm
@@ -3,7 +3,7 @@ package MIP::Recipes::Analysis::Vt;
 use Carp;
 use charnames qw{ :full :short };
 use English qw{ -no_match_vars };
-use File::Spec::Functions qw{ catdir catfile };
+use File::Spec::Functions qw{ catdir catfile splitpath };
 use open qw{ :encoding(UTF-8) :std };
 use Params::Check qw{ allow check last_error };
 use strict;
