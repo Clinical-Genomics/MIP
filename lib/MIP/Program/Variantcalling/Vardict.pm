8331aed7dbe26b7ad9a41f2098891fed0452627e
hassanfa <hassan.foroughi@gmail.com>
2017-12-21 14:37:08 +0100

    moved var2vcf into vardict module, removed individual modules for var2vcf, and test modules updated accordingly

--- lib/MIP/Program/Variantcalling/Vardict.pm
+++ lib/MIP/Program/Variantcalling/Vardict.pm
@@ -22,10 +22,11 @@ BEGIN {
     use base qw{ Exporter };
 
     # Set the version for version checking
-    our $VERSION = 1.00;
+    our $VERSION = 1.01;
 
     # Functions and variables which can be optionally exported
-    our @EXPORT_OK = qw{ vardict };
+    our @EXPORT_OK =
+      qw{ vardict vardict_var2vcf_paired vardict_var2vcf_single };
 }
 
 ## Constants
@@ -75,9 +76,9 @@ sub vardict {
         af_threshold => {
             ## Exactly 2 decimal points after 0 or 1
             allow       => qr/ ^0.\d{1,2}$ | ^1$ /xsm,
-            defined  => 1,
-            default  => 0.01,
-            required => 1,
+            defined     => 1,
+            default     => 0.01,
+            required    => 1,
             store       => \$af_threshold,
             strict_type => 1,
         },
@@ -183,8 +184,178 @@ sub vardict {
 
     unix_write_to_file(
         {
-            FILEHANDLE   => $FILEHANDLE,
             commands_ref => \@commands,
+            FILEHANDLE   => $FILEHANDLE,
+            separator    => $SPACE,
+
+        }
+    );
+    return @commands;
+}
+
+sub vardict_var2vcf_single {
+
+## Function : Perl wrapper for var2vcf_singlelsls, converting variant output to vcf. Based on vardict 2017.09.24
+## Returns  : @commands
+## Arguments: $af_threshold           => Threshold for allele frequency
+##          : $FILEHANDLE             => Filehandle to write to
+##          : $sample_name            => Sample name to be used directly, will overwrite -n option
+##          : $stderrfile_path        => Stderrfile path
+##          : $stderrfile_path_append => Append stderr info to file path
+##          : $stdoutfile_path        => Stdoutfile path
+
+    my ($arg_href) = @_;
+
+    ## Flatten argument(s)
+    my $FILEHANDLE;
+    my $sample_name;
+    my $stderrfile_path;
+    my $stderrfile_path_append;
+    my $stdoutfile_path;
+
+    ## Default(s)
+    my $af_threshold;
+
+    my $tmpl = {
+        af_threshold => {
+            ## Exactly 2 decimal points after 0 or 1
+            allow       => qr/ ^0.\d{1,2}$ | ^1$ /xsm,
+            defined     => 1,
+            default     => 0.01,
+            required    => 1,
+            store       => \$af_threshold,
+            strict_type => 1,
+        },
+        sample_name => {
+            allow       => qr/ ^\w+$ /xsm,
+            required    => 1,
+            store       => \$sample_name,
+            strict_type => 1,
+        },
+        FILEHANDLE => {
+            store => \$FILEHANDLE,
+        },
+        stderrfile_path => {
+            store       => \$stderrfile_path,
+            strict_type => 1,
+        },
+        stderrfile_path_append => {
+            store       => \$stderrfile_path_append,
+            strict_type => 1,
+        },
+        stdoutfile_path => {
+            store       => \$stdoutfile_path,
+            strict_type => 1,
+        },
+    };
+
+    check( $tmpl, $arg_href, 1 ) or croak q{Could not parse arguments!};
+
+    ## Stores commands depending on input parameters
+    my @commands = q{var2vcf_valid.pl};
+
+    push @commands, q{-f} . $SPACE . $af_threshold;
+
+    push @commands, q{-N} . $SPACE . $sample_name;
+
+    push @commands,
+      unix_standard_streams(
+        {
+            stderrfile_path        => $stderrfile_path,
+            stderrfile_path_append => $stderrfile_path_append,
+            stdoutfile_path        => $stdoutfile_path,
+        }
+      );
+
+    unix_write_to_file(
+        {
+            commands_ref => \@commands,
+            FILEHANDLE   => $FILEHANDLE,
+            separator    => $SPACE,
+
+        }
+    );
+    return @commands;
+}
+
+sub vardict_var2vcf_paired {
+
+## Function : Perl wrapper for var2vcf_paired, converting variant output to vcf. Based on vardict 2017.09.24
+## Returns  : @commands
+## Arguments: $af_threshold           => Threshold for allele frequency
+##          : $FILEHANDLE             => Filehandle to write to
+##          : $sample_name            => Sample name to be used directly, will overwrite -n option
+##          : $stderrfile_path        => Stderrfile path
+##          : $stderrfile_path_append => Append stderr info to file path
+##          : $stdoutfile_path        => Stdoutfile path
+
+    my ($arg_href) = @_;
+
+    ## Flatten argument(s)
+    my $FILEHANDLE;
+    my $sample_name;
+    my $stderrfile_path;
+    my $stderrfile_path_append;
+    my $stdoutfile_path;
+
+    ## Default(s)
+    my $af_threshold;
+
+    my $tmpl = {
+        af_threshold => {
+            ## Exactly 2 decimal points after 0 or 1
+            allow       => qr/ ^0.\d{1,2}$ | ^1$ /xsm,
+            defined     => 1,
+            default     => 0.01,
+            required    => 1,
+            store       => \$af_threshold,
+            strict_type => 1,
+        },
+        sample_name => {
+            allow       => qr/ ^\w+$ /xsm,
+            required    => 1,
+            store       => \$sample_name,
+            strict_type => 1,
+        },
+        FILEHANDLE => {
+            store => \$FILEHANDLE,
+        },
+        stderrfile_path => {
+            store       => \$stderrfile_path,
+            strict_type => 1,
+        },
+        stderrfile_path_append => {
+            store       => \$stderrfile_path_append,
+            strict_type => 1,
+        },
+        stdoutfile_path => {
+            store       => \$stdoutfile_path,
+            strict_type => 1,
+        },
+    };
+
+    check( $tmpl, $arg_href, 1 ) or croak q{Could not parse arguments!};
+
+    ## Stores commands depending on input parameters
+    my @commands = q{var2vcf_paired.pl};
+
+    push @commands, q{-f} . $SPACE . $af_threshold;
+
+    push @commands, q{-N} . $SPACE . $sample_name;
+
+    push @commands,
+      unix_standard_streams(
+        {
+            stderrfile_path        => $stderrfile_path,
+            stderrfile_path_append => $stderrfile_path_append,
+            stdoutfile_path        => $stdoutfile_path,
+        }
+      );
+
+    unix_write_to_file(
+        {
+            commands_ref => \@commands,
+            FILEHANDLE   => $FILEHANDLE,
             separator    => $SPACE,
 
         }
