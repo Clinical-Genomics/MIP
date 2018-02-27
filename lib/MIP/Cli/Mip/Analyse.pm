68856ab2febf430061c72884ebb75ac5e6dfae7e
henrikstranneheim <henrik.stranneheim@scilifelab.se>
2018-02-27 16:57:17 +0100

    Modified syntax and flags
    - Added enum for checking valid values per flag
    - Added cmd_flag only for long flag names

--- lib/MIP/Cli/Mip/Analyse.pm
+++ lib/MIP/Cli/Mip/Analyse.pm
@@ -11,6 +11,7 @@ use warnings qw{ FATAL utf8 };
 ## CPANM
 use autodie qw{ :all };
 use MooseX::App::Command;
+use Moose::Util::TypeConstraints;
 
 our $VERSION = 0.01;
 
@@ -47,121 +48,147 @@ sub _build_usage {
 
     option(
         q{analysis_type} => (
-            cmd_aliases => [qw{ at }],
-            cmd_flag    => q{analysis_type},
-            documentation =>
-q{Type of analysis (defaults to 'wgs'; Valid entries: 'wgs', 'wes', 'wts', 'cancer'; sample_id=analysis_type)},
-            is       => q{rw},
-            isa      => q{HashRef},
+            cmd_aliases   => [qw{ at }],
+            cmd_tags      => [q{sample_id=analysis_type}],
+            documentation => q{Type of analysis},
+            is            => q{rw},
+            isa           => q{HashRef},
         )
     );
 
     option(
         q{dry_run_all} => (
             cmd_aliases => [qw{ dra }],
-            cmd_flag    => q{dry_run_all},
             documentation =>
               q{Sets all programs to dry run mode i.e. no sbatch submission},
-            is       => q{rw},
-            isa      => q{Bool},
+            is  => q{rw},
+            isa => q{Bool},
         )
     );
 
     option(
         q{config_file_analysis} => (
             cmd_aliases => [qw{ cfa }],
-            cmd_flag    => q{config_file_analysis},
             documentation =>
-              q{Write YAML configuration file the analysis parameters (defaults to "")},
-            is       => q{rw},
-            isa      => q{Str},
+              q{Write YAML configuration file the analysis parameters},
+            is  => q{rw},
+            isa => q{Str},
         )
     );
 
     option(
         q{family_id} => (
-            cmd_aliases => [qw{ fam }],
-            cmd_flag    => q{family_id},
-            documentation =>
-              q{Group id of samples to be compared (defaults to "")},
-            is       => q{rw},
-            isa      => q{Str},
-            required => 1,
+            cmd_aliases   => [qw{ fam }],
+            documentation => q{Group id of samples to be compared},
+            is            => q{rw},
+            isa           => q{Str},
+            required      => 1,
+        )
+    );
+
+    option(
+        q{human_genome_reference} => (
+            cmd_aliases   => [qw{ hgr }],
+            cmd_tags      => [q{Fasta}],
+            documentation => q{Human genome reference},
+            is            => q{rw},
+            isa           => q{Str},
         )
     );
 
     option(
         q{infile_dirs} => (
             cmd_aliases   => [qw{ ifd }],
-            cmd_flag      => q{infile_dirs},
-            documentation => q{Infile directory(s); infile_dirs=sample_id},
+            cmd_tags      => [q{infile_dirs=sample_id}],
+            documentation => q{Infile directory(s)},
             is            => q{rw},
             isa           => q{HashRef},
+        )
+    );
+
+    option(
+        q{outaligner_dir} => (
+            cmd_aliases => [qw{ ald }],
+            documentation =>
+q{Sets which aligner out directory was used for alignment in previous analysis},
+            is  => q{rw},
+            isa => enum( [qw{ bwa star }] ),
         )
     );
 
     option(
         q{outdata_dir} => (
-            cmd_aliases => [qw{ odd }],
-            cmd_flag    => q{outdata_dir},
-            documentation =>
-              q{Data output directory},
-            is       => q{rw},
-            isa      => q{Str},
+            cmd_aliases   => [qw{ odd }],
+            documentation => q{Data output directory},
+            is            => q{rw},
+            isa           => q{Str},
         )
     );
 
     option(
         q{outscript_dir} => (
-            cmd_aliases => [qw{ osd }],
-            cmd_flag    => q{outscript_dir},
-            documentation =>
-              q{Script files (.sh) output directory},
-            is       => q{rw},
-            isa      => q{Str},
+            cmd_aliases   => [qw{ osd }],
+            documentation => q{Script files (.sh) output directory},
+            is            => q{rw},
+            isa           => q{Str},
+        )
+    );
+
+    option(
+        q{pedigree_file} => (
+            cmd_aliases   => [qw{ ped }],
+            documentation => q{Meta data on samples},
+            is            => q{rw},
+            isa           => q{Str},
+        )
+    );
+
+    option(
+        q{platform} => (
+            cmd_aliases   => [qw{ pla }],
+            documentation => q{Platform/technology used to produce the reads},
+            is            => q{rw},
+            isa           => enum( [qw{ ILLUMINA }] ),
         )
     );
 
     option(
         q{project_id} => (
-            cmd_aliases => [qw{ pro }],
-            cmd_flag    => q{project_id},
-            documentation =>
-              q{Project id},
-            is       => q{rw},
-            isa      => q{Str},
+            cmd_aliases   => [qw{ pro }],
+            documentation => q{Project id},
+            is            => q{rw},
+            isa           => q{Str},
         )
     );
 
     option(
         q{reference_dir} => (
-            cmd_aliases => [qw{ rd }],
-            cmd_flag    => q{reference_dir},
-            documentation =>
-              q{Reference(s) directory},
-            is       => q{rw},
-            isa      => q{Str},
+            cmd_aliases   => [qw{ rd }],
+            documentation => q{Reference(s) directory},
+            is            => q{rw},
+            isa           => q{Str},
         )
     );
 
     option(
         q{supported_capture_kit} => (
-            cmd_aliases   => [qw{ sck }],
-            cmd_flag      => q{supported_capture_kit},
-            documentation => q{Set the capture kit acronym shortcut in pedigree file},
-            is            => q{rw},
-            isa           => q{HashRef},
+            cmd_aliases => [qw{ sck }],
+            cmd_flag    => q{supported_capture_kit},
+            cmd_tags    => [q{acronym=file.bed}],
+            documentation =>
+              q{Set the capture kit acronym shortcut in pedigree file},
+            is  => q{rw},
+            isa => q{HashRef},
         )
     );
 
     option(
         q{sample_ids} => (
             cmd_aliases => [qw{ spi }],
-            cmd_flag    => q{sample_ids},
             documentation =>
               q{Sets all programs to dry run mode i.e. no sbatch submission},
-            is       => q{rw},
-            isa      => q{ArrayRef},
+            is  => q{rw},
+            isa => q{ArrayRef},
         )
     );
     return;
