bc82dad39ad6af3770e638ef1a9c6aa8fcec1c82
jemten <jemten@users.noreply.github.com>
2018-03-23 17:18:42 +0100

    corrections in pipeline recipe

--- lib/MIP/Recipes/Pipeline/Rare_disease.pm
+++ lib/MIP/Recipes/Pipeline/Rare_disease.pm
@@ -161,7 +161,7 @@ sub pipeline_rare_disease {
     use MIP::Recipes::Analysis::Delly_call qw{ analysis_delly_call };
     use MIP::Recipes::Analysis::Delly_reformat qw{ analysis_delly_reformat };
     use MIP::Recipes::Analysis::Endvariantannotationblock
-    use MIP::Recipes::Analysis::Expansionhunter
+      use MIP::Recipes::Analysis::Expansionhunter
       qw{ analysis_expansionhunter };
     use MIP::Recipes::Analysis::Fastqc qw{ analysis_fastqc };
     use MIP::Recipes::Analysis::Freebayes qw { analysis_freebayes_calling };
@@ -843,8 +843,9 @@ sub pipeline_rare_disease {
 
         $log->info(q{[ExpansionHunter]});
 
-        my $cnvnator_program_name = q{expansionhunter};
+        my $expansionhunter_program_name = q{expansionhunter};
 
+      SAMPLE_ID:
         foreach my $sample_id ( @{ $active_parameter_href->{sample_ids} } ) {
 
             ## Assign directories
@@ -855,21 +856,21 @@ sub pipeline_rare_disease {
             my $outsample_directory = catdir(
                 $active_parameter_href->{outdata_dir},
                 $sample_id, $active_parameter_href->{outaligner_dir},
-                $cnvnator_program_name
+                $expansionhunter_program_name
             );
 
             analysis_expansionhunter(
                 {
-                    parameter_href          => $parameter_href,
                     active_parameter_href   => $active_parameter_href,
-                    sample_info_href        => $sample_info_href,
                     file_info_href          => $file_info_href,
                     infile_lane_prefix_href => $infile_lane_prefix_href,
-                    job_id_href             => $job_id_href,
-                    sample_id               => $sample_id,
                     insample_directory      => $insample_directory,
+                    job_id_href             => $job_id_href,
                     outsample_directory     => $outsample_directory,
+                    parameter_href          => $parameter_href,
                     program_name            => $cnvnator_program_name,
+                    sample_id               => $sample_id,
+                    sample_info_href        => $sample_info_href,
                 }
             );
         }
