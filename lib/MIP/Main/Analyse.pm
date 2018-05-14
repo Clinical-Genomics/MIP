9eb02cd694c74fa1f4b0656b5b2bb21511816bfc
jemten <jemten@users.noreply.github.com>
2018-05-14 14:56:50 +0200

    GATK SplitNCigarReads recipe, resolves #494

--- lib/MIP/Main/Analyse.pm
+++ lib/MIP/Main/Analyse.pm
@@ -666,9 +666,7 @@ sub mip_analyse {
     if ( exists $active_parameter{pvarianteffectpredictor} ) {
         check_vep_directories(
             {
-                log                 => $log,
-                vep_directory_cache => $active_parameter{vep_directory_cache},
-                vep_directory_path  => $active_parameter{vep_directory_path},
+                log => $log,
             }
         );
     }
