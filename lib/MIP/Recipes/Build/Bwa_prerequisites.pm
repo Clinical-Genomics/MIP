b3ae1af4f9fbbb5f0cdfe2ca7ebb737bc2a19ab4
henrikstranneheim <henrik.stranneheim@scilifelab.se>
2018-06-28 20:40:53 +0200

    Removed pprogram name from bwa_prerequisites

--- lib/MIP/Recipes/Build/Bwa_prerequisites.pm
+++ lib/MIP/Recipes/Build/Bwa_prerequisites.pm
@@ -1,18 +1,18 @@
 package MIP::Recipes::Build::Bwa_prerequisites;
 
+use Carp;
+use charnames qw{ :full :short };
+use English qw{ -no_match_vars };
+use File::Spec::Functions qw{ catdir catfile };
+use open qw{ :encoding(UTF-8) :std };
+use Params::Check qw{ check allow last_error };
 use strict;
+use utf8;
 use warnings;
 use warnings qw{ FATAL utf8 };
-use utf8;
-use open qw{ :encoding(UTF-8) :std };
-use autodie qw{ :all };
-use charnames qw{ :full :short };
-use Carp;
-use English qw{ -no_match_vars };
-use Params::Check qw{ check allow last_error };
-use File::Spec::Functions qw{ catdir catfile };
 
 ## CPANM
+use autodie qw{ :all };
 use Readonly;
 
 BEGIN {
@@ -21,7 +21,7 @@ BEGIN {
     use base qw{ Exporter };
 
     # Set the version for version checking
-    our $VERSION = 1.00;
+    our $VERSION = 1.01;
 
     # Functions and variables which can be optionally exported
     our @EXPORT_OK = qw{ build_bwa_prerequisites };
@@ -37,127 +37,125 @@ sub build_bwa_prerequisites {
 
 ## Function : Creates the Bwa prerequisites
 ## Returns  :
-## Arguments: $parameter_href                       => Parameter hash {REF}
-##          : $active_parameter_href                => Active parameters for this analysis hash {REF}
-##          : $sample_info_href                     => Info on samples and family hash {REF}
+## Arguments: $active_parameter_href                => Active parameters for this analysis hash {REF}
+##          : $bwa_build_reference_file_endings_ref => The bwa reference associated file endings {REF}
+##          : $family_id                            => Family id
 ##          : $file_info_href                       => File info hash {REF}
+##          : $human_genome_reference               => Human genome reference
 ##          : $infile_lane_prefix_href              => Infile(s) without the ".ending" {REF}
 ##          : $job_id_href                          => Job id hash {REF}
-##          : $bwa_build_reference_file_endings_ref => The bwa reference associated file endings {REF}
-##          : $family_id                            => Family ID {REF}
-##          : $outaligner_dir_ref                   => The outaligner_dir used in the analysis
+##          : $parameter_href                       => Parameter hash {REF}
 ##          : $program_name                         => Program name
-##          : $family_id                            => Family id
-##          : $temp_directory                       => Temporary directory
 ##          : $outaligner_dir                       => Outaligner_dir used in the analysis
-##          : $human_genome_reference               => Human genome reference
+##          : $sample_info_href                     => Info on samples and family hash {REF}
+##          : $temp_directory                       => Temporary directory
 
     my ($arg_href) = @_;
 
     ## Flatten argument(s)
-    my $parameter_href;
     my $active_parameter_href;
-    my $sample_info_href;
+    my $bwa_build_reference_file_endings_ref;
     my $file_info_href;
     my $infile_lane_prefix_href;
     my $job_id_href;
-    my $bwa_build_reference_file_endings_ref;
+    my $parameter_href;
     my $program_name;
+    my $sample_info_href;
 
     ## Default(s)
     my $family_id;
-    my $temp_directory;
-    my $outaligner_dir;
     my $human_genome_reference;
+    my $outaligner_dir;
+    my $temp_directory;
 
     my $tmpl = {
-        parameter_href => {
-            required    => 1,
-            defined     => 1,
-            default     => {},
-            strict_type => 1,
-            store       => \$parameter_href,
-        },
         active_parameter_href => {
-            required    => 1,
-            defined     => 1,
             default     => {},
-            strict_type => 1,
+            defined     => 1,
+            required    => 1,
             store       => \$active_parameter_href,
-        },
-        sample_info_href => {
-            required    => 1,
-            defined     => 1,
-            default     => {},
             strict_type => 1,
-            store       => \$sample_info_href,
-        },
-        file_info_href => {
-            required    => 1,
-            defined     => 1,
-            default     => {},
-            strict_type => 1,
-            store       => \$file_info_href,
-        },
-        infile_lane_prefix_href => {
-            required    => 1,
-            defined     => 1,
-            default     => {},
-            strict_type => 1,
-            store       => \$infile_lane_prefix_href,
-        },
-        job_id_href => {
-            required    => 1,
-            defined     => 1,
-            default     => {},
-            strict_type => 1,
-            store       => \$job_id_href,
         },
         bwa_build_reference_file_endings_ref => {
-            required    => 1,
-            defined     => 1,
             default     => [],
-            strict_type => 1,
-            store       => \$bwa_build_reference_file_endings_ref
-        },
-        program_name => {
-            required    => 1,
             defined     => 1,
+            required    => 1,
+            store       => \$bwa_build_reference_file_endings_ref,
             strict_type => 1,
-            store       => \$program_name,
         },
         family_id => {
             default     => $arg_href->{active_parameter_href}{family_id},
-            strict_type => 1,
             store       => \$family_id,
-        },
-        temp_directory => {
-            default     => $arg_href->{active_parameter_href}{temp_directory},
             strict_type => 1,
-            store       => \$temp_directory,
         },
-        outaligner_dir => {
-            default     => $arg_href->{active_parameter_href}{outaligner_dir},
+        file_info_href => {
+            default     => {},
+            defined     => 1,
+            required    => 1,
+            store       => \$file_info_href,
             strict_type => 1,
-            store       => \$outaligner_dir,
         },
         human_genome_reference => {
             default =>
               $arg_href->{active_parameter_href}{human_genome_reference},
+            store       => \$human_genome_reference,
             strict_type => 1,
-            store       => \$human_genome_reference
+        },
+        infile_lane_prefix_href => {
+            default     => {},
+            defined     => 1,
+            required    => 1,
+            store       => \$infile_lane_prefix_href,
+            strict_type => 1,
+        },
+        job_id_href => {
+            default     => {},
+            defined     => 1,
+            required    => 1,
+            store       => \$job_id_href,
+            strict_type => 1,
+        },
+        parameter_href => {
+            default     => {},
+            defined     => 1,
+            required    => 1,
+            store       => \$parameter_href,
+            strict_type => 1,
+        },
+        program_name => {
+            defined     => 1,
+            required    => 1,
+            store       => \$program_name,
+            strict_type => 1,
+        },
+        outaligner_dir => {
+            default     => $arg_href->{active_parameter_href}{outaligner_dir},
+            store       => \$outaligner_dir,
+            strict_type => 1,
+        },
+        sample_info_href => {
+            default     => {},
+            defined     => 1,
+            required    => 1,
+            store       => \$sample_info_href,
+            strict_type => 1,
+        },
+        temp_directory => {
+            default     => $arg_href->{active_parameter_href}{temp_directory},
+            store       => \$temp_directory,
+            strict_type => 1,
         },
     };
 
     check( $tmpl, $arg_href, 1 ) or croak q{Could not parse arguments!};
 
-    use MIP::Program::Alignment::Bwa qw{ bwa_index };
     use MIP::Language::Shell qw{ check_exist_and_move_file };
+    use MIP::Processmanagement::Slurm_processes
+      qw{ slurm_submit_job_no_dependency_add_to_samples };
+    use MIP::Program::Alignment::Bwa qw{ bwa_index };
     use MIP::Recipes::Build::Human_genome_prerequisites
       qw{ build_human_genome_prerequisites };
     use MIP::Script::Setup_script qw{ setup_script };
-    use MIP::Processmanagement::Slurm_processes
-      qw{ slurm_submit_job_no_dependency_add_to_samples };
 
     ## Constants
     Readonly my $MAX_RANDOM_NUMBER => 100_00;
@@ -166,12 +164,11 @@ sub build_bwa_prerequisites {
     ## Retrieve logger object
     my $log = Log::Log4perl->get_logger(q{MIP});
 
-    ## Set MIP program name
-    my $mip_program_name = q{p} . $program_name;
-    my $mip_program_mode = $active_parameter_href->{$mip_program_name};
+    ## Set program mode
+    my $program_mode = $active_parameter_href->{$program_name};
 
     ## Unpack parameters
-    my $job_id_chain = $parameter_href->{$mip_program_name}{chain};
+    my $job_id_chain = $parameter_href->{$program_name}{chain};
 
     ## FILEHANDLES
     # Create anonymous filehandle
@@ -184,27 +181,27 @@ sub build_bwa_prerequisites {
     my ($file_path) = setup_script(
         {
             active_parameter_href => $active_parameter_href,
-            job_id_href           => $job_id_href,
-            FILEHANDLE            => $FILEHANDLE,
             directory_id          => $family_id,
-            program_name          => $program_name,
+            FILEHANDLE            => $FILEHANDLE,
+            job_id_href           => $job_id_href,
             program_directory     => $outaligner_dir,
+            program_name          => $program_name,
             process_time          => $PROCESSING_TIME,
         }
     );
 
     build_human_genome_prerequisites(
         {
-            parameter_href          => $parameter_href,
             active_parameter_href   => $active_parameter_href,
-            sample_info_href        => $sample_info_href,
+            FILEHANDLE              => $FILEHANDLE,
             file_info_href          => $file_info_href,
             infile_lane_prefix_href => $infile_lane_prefix_href,
             job_id_href             => $job_id_href,
-            program_name            => $program_name,
-            FILEHANDLE              => $FILEHANDLE,
             log                     => $log,
+            parameter_href          => $parameter_href,
+            program_name            => $program_name,
             random_integer          => $random_integer,
+            sample_info_href        => $sample_info_href,
         }
     );
 
@@ -216,14 +213,15 @@ sub build_bwa_prerequisites {
               . $program_name );
 
         say {$FILEHANDLE} q{## Building BWA index};
+
         ## Get parameters
         my $prefix = $human_genome_reference . $UNDERSCORE . $random_integer;
         bwa_index(
             {
-                prefix                 => $prefix,
                 construction_algorithm => q{bwtsw},
-                reference_genome       => $human_genome_reference,
                 FILEHANDLE             => $FILEHANDLE,
+                prefix                 => $prefix,
+                reference_genome       => $human_genome_reference,
             }
         );
         say {$FILEHANDLE} $NEWLINE;
@@ -251,16 +249,16 @@ sub build_bwa_prerequisites {
 
     close $FILEHANDLE or $log->logcroak(q{Could not close FILEHANDLE});
 
-    if ( $mip_program_mode == 1 ) {
+    if ( $program_mode == 1 ) {
 
         slurm_submit_job_no_dependency_add_to_samples(
             {
-                job_id_href      => $job_id_href,
-                sample_ids_ref   => \@{ $active_parameter_href->{sample_ids} },
                 family_id        => $family_id,
-                path             => $job_id_chain,
-                sbatch_file_name => $file_path,
+                job_id_href      => $job_id_href,
                 log              => $log,
+                path             => $job_id_chain,
+                sample_ids_ref   => \@{ $active_parameter_href->{sample_ids} },
+                sbatch_file_name => $file_path,
             }
         );
     }
