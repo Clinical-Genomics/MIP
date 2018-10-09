50332d88d32c4f558161587e63f3a396071521ae
henrikstranneheim <henrik.stranneheim@scilifelab.se>
2018-10-09 08:44:30 +0200

    Added io to rtg_vcfeval

--- lib/MIP/Recipes/Analysis/Rtg_vcfeval.pm
+++ lib/MIP/Recipes/Analysis/Rtg_vcfeval.pm
@@ -21,7 +21,7 @@ BEGIN {
     use base qw{ Exporter };
 
     # Set the version for version checking
-    our $VERSION = 1.01;
+    our $VERSION = 1.02;
 
     # Functions and variables which can be optionally exported
     our @EXPORT_OK = qw{ analysis_rtg_vcfeval };
@@ -38,12 +38,10 @@ sub analysis_rtg_vcfeval {
 ## Function : Evaluation of vcf variants using rtg
 ## Returns  :
 ## Arguments: $active_parameter_href   => Active parameters for this analysis hash {REF}
-##          : $call_type               => Variant call type
 ##          : $family_id               => Family id
 ##          : $file_info_href          => File_info hash {REF}
 ##          : $infile_lane_prefix_href => Infile(s) without the ".ending" {REF}
 ##          : $job_id_href             => Job id hash {REF}
-##          : $outaligner_dir          => Outaligner_dir used in the analysis
 ##          : $parameter_href          => Parameter hash {REF}
 ##          : $program_name            => Program name
 ##          : $sample_id               => Sample id
@@ -63,9 +61,7 @@ sub analysis_rtg_vcfeval {
     my $sample_info_href;
 
     ## Default(s)
-    my $call_type;
     my $family_id;
-    my $outaligner_dir;
     my $temp_directory;
 
     my $tmpl = {
@@ -76,8 +72,6 @@ sub analysis_rtg_vcfeval {
             store       => \$active_parameter_href,
             strict_type => 1,
         },
-        call_type =>
-          { default => q{BOTH}, store => \$call_type, strict_type => 1, },
         family_id => {
             default     => $arg_href->{active_parameter_href}{family_id},
             store       => \$family_id,
@@ -102,11 +96,6 @@ sub analysis_rtg_vcfeval {
             defined     => 1,
             required    => 1,
             store       => \$job_id_href,
-            strict_type => 1,
-        },
-        outaligner_dir => {
-            default     => $arg_href->{active_parameter_href}{outaligner_dir},
-            store       => \$outaligner_dir,
             strict_type => 1,
         },
         parameter_href => {
@@ -144,9 +133,10 @@ sub analysis_rtg_vcfeval {
 
     check( $tmpl, $arg_href, 1 ) or croak q{Could not parse arguments!};
 
-    use MIP::Get::File qw{ get_file_suffix };
-    use MIP::Get::Parameter qw{ get_module_parameters };
+    use MIP::Get::File qw{ get_io_files };
+    use MIP::Get::Parameter qw{ get_module_parameters get_program_attributes };
     use MIP::Gnu::Coreutils qw{ gnu_rm  };
+    use MIP::Parse::File qw{ parse_io_outfiles };
     use MIP::Program::Qc::Rtg qw{ rtg_vcfeval };
     use MIP::Program::Variantcalling::Bcftools
       qw{ bcftools_rename_vcf_samples bcftools_view_and_index_vcf };
@@ -158,14 +148,35 @@ sub analysis_rtg_vcfeval {
     ## Return if not a nist_id sample
     return if ( not $sample_id =~ /$active_parameter_href->{nist_id}/sxm );
 
+    ### PREPROCESSING:
+
     ## Retrieve logger object
     my $log = Log::Log4perl->get_logger(q{MIP});
 
-    ## Set program mode
-    my $program_mode = $active_parameter_href->{$program_name};
-
     ## Unpack parameters
-    my $job_id_chain = $parameter_href->{$program_name}{chain};
+## Get the io infiles per chain and id
+    my %io = get_io_files(
+        {
+            id             => $family_id,
+            file_info_href => $file_info_href,
+            parameter_href => $parameter_href,
+            program_name   => $program_name,
+            stream         => q{in},
+            temp_directory => $temp_directory,
+        }
+    );
+    my $infile_name_prefix = $io{in}{file_name_prefix};
+    my $infile_path        = $io{in}{file_path};
+
+    my $job_id_chain = get_program_attributes(
+        {
+            parameter_href => $parameter_href,
+            program_name   => $program_name,
+            attribute      => q{chain},
+        }
+    );
+    my $nist_id      = $active_parameter_href->{nist_id};
+    my $program_mode = $active_parameter_href->{$program_name};
     my ( $core_number, $time, @source_environment_cmds ) =
       get_module_parameters(
         {
@@ -174,52 +185,45 @@ sub analysis_rtg_vcfeval {
         }
       );
 
+    ## Set and get the io files per chain, id and stream
+    %io = (
+        %io,
+        parse_io_outfiles(
+            {
+                chain_id         => $job_id_chain,
+                id               => $family_id,
+                file_info_href   => $file_info_href,
+                file_name_prefix => $infile_name_prefix,
+                iterators_ref    => [$nist_id],
+                outdata_dir      => $active_parameter_href->{outdata_dir},
+                parameter_href   => $parameter_href,
+                program_name     => $program_name,
+                temp_directory   => $temp_directory,
+            }
+        )
+    );
+
+    my $outdir_path_prefix  = $io{out}{dir_path_prefix};
+    my $outfile_path_prefix = $io{out}{file_path_prefix};
+
     ## Filehandles
     # Create anonymous filehandle
     my $FILEHANDLE = IO::Handle->new();
 
-    ## Assign directories
-    my $infamily_directory = catdir( $active_parameter_href->{outdata_dir},
-        $family_id, $outaligner_dir );
-    my $outfamily_directory = catfile( $active_parameter_href->{outdata_dir},
-        $family_id, $outaligner_dir, $program_name );
-
-    ## Assign file_tags
-    my $infile_tag =
-      $file_info_href->{$family_id}{gatk_combinevariantcallsets}{file_tag};
-
-    my $infile_prefix = $family_id . $infile_tag . $call_type;
-
-    ## Get infile_suffix from baserecalibration jobid chain
-    my $infile_suffix = get_file_suffix(
-        {
-            jobid_chain =>
-              $parameter_href->{gatk_combinevariantcallsets}{chain},
-            parameter_href => $parameter_href,
-            suffix_key     => q{variant_file_suffix},
-        }
-    );
-
-    ## Files
-    my $infile_name = $infile_prefix . $infile_suffix;
-
-    ## Paths
-    my $infile_path      = catfile( $infamily_directory, $infile_name );
-    my $file_path_prefix = catfile( $temp_directory,     $infile_prefix );
-    my $nist_file_path   = catfile( $temp_directory,     q{nist} );
+    my $nist_file_path = catfile( $temp_directory, q{nist} );
 
     ## Creates program directories (info & programData & programScript), program script filenames and writes sbatch header
     my ( $file_path, $program_info_path ) = setup_script(
         {
-            active_parameter_href => $active_parameter_href,
-            core_number           => $core_number,
-            directory_id          => $family_id,
-            FILEHANDLE            => $FILEHANDLE,
-            job_id_href           => $job_id_href,
-            log                   => $log,
-            process_time          => $time,
-            program_directory     => catfile( $outaligner_dir, $program_name ),
-            program_name          => $program_name,
+            active_parameter_href           => $active_parameter_href,
+            core_number                     => $core_number,
+            directory_id                    => $family_id,
+            FILEHANDLE                      => $FILEHANDLE,
+            job_id_href                     => $job_id_href,
+            log                             => $log,
+            process_time                    => $time,
+            program_directory               => $program_name,
+            program_name                    => $program_name,
             source_environment_commands_ref => \@source_environment_cmds,
         }
     );
@@ -234,7 +238,7 @@ sub analysis_rtg_vcfeval {
             outfile_path_prefix => $nist_file_path . $UNDERSCORE . q{refrm},
             output_type         => q{z},
             temp_directory      => $temp_directory,
-            sample_ids_ref      => [ $active_parameter_href->{nist_id} ],
+            sample_ids_ref      => [$nist_id],
         }
     );
 
@@ -245,13 +249,13 @@ sub analysis_rtg_vcfeval {
             index               => 1,
             index_type          => q{tbi},
             infile_path         => $infile_path,
-            outfile_path_prefix => $file_path_prefix,
+            outfile_path_prefix => $outfile_path_prefix,
             output_type         => q{z},
         }
     );
 
     say {$FILEHANDLE} q{## Remove potential old Rtg vcfeval outdir};
-    my $rtg_outdirectory_path = catfile( $outfamily_directory, $sample_id );
+    my $rtg_outdirectory_path = catfile( $outdir_path_prefix, $sample_id );
     gnu_rm(
         {
             FILEHANDLE  => $FILEHANDLE,
@@ -268,12 +272,12 @@ sub analysis_rtg_vcfeval {
             baselinefile_path => $nist_file_path
               . $UNDERSCORE
               . q{refrm.vcf.gz},
-            callfile_path => $file_path_prefix . $DOT . q{vcf.gz},
+            callfile_path => $outfile_path_prefix . $DOT . q{vcf.gz},
             eval_region_file_path =>
               $active_parameter_href->{nist_high_confidence_call_set_bed},
             FILEHANDLE           => $FILEHANDLE,
             outputdirectory_path => $rtg_outdirectory_path,
-            sample_id            => $active_parameter_href->{nist_id},
+            sample_id            => $nist_id,
             sdf_template_file_path =>
               $active_parameter_href->{rtg_vcfeval_reference_genome}
               . $file_info_href->{rtg_vcfeval_reference_genome}[0]
