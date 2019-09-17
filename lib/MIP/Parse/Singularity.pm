016d3114784a334120fa04b07c290f2ea0ec8c1d
jemten <jemten@users.noreply.github.com>
2019-09-17 20:04:05 +0200

    feat(singularity): revamp

--- lib/MIP/Parse/Singularity.pm
+++ lib/MIP/Parse/Singularity.pm
@@ -4,7 +4,7 @@ use 5.026;
 use Carp;
 use charnames qw{ :full :short };
 use English qw{ -no_match_vars };
-use File::Spec::Functions qw{ catdir catfile splitdir };
+use File::Spec::Functions qw{ catdir splitdir };
 use open qw{ :encoding(UTF-8) :std };
 use Params::Check qw{ allow check last_error };
 use strict;
@@ -16,87 +16,15 @@ use warnings qw{ FATAL utf8 };
 use autodie qw{ :all };
 use Readonly;
 
-## MIPs lib/
-use MIP::Constants qw{ $SPACE @SINGULARITY_BIND_PATHS %SINGULARITY_CONTAINER };
-
 BEGIN {
     require Exporter;
     use base qw{ Exporter };
 
     # Set the version for version checking
-    our $VERSION = 1.00;
+    our $VERSION = 1.01;
 
     # Functions and variables which can be optionally exported
-    our @EXPORT_OK = qw{ parse_commands_for_singularity parse_sing_bind_paths };
-}
-
-sub parse_commands_for_singularity {
-
-## Function : Parse command array from unix_write_to_file
-## Returns  :
-## Arguments: $commands_ref => Commands to write to file
-
-    my ($arg_href) = @_;
-
-    ## Flatten argument(s)
-    my $commands_ref;
-
-    my $tmpl = {
-        commands_ref => {
-            default     => [],
-            defined     => 1,
-            required    => 1,
-            store       => \$commands_ref,
-            strict_type => 1,
-        },
-    };
-
-    check( $tmpl, $arg_href, 1 ) or croak q{Could not parse arguments!};
-
-    use MIP::Program::Singularity qw{ singularity_exec };
-
-    # Return if empty command array
-    return if ( not @{$commands_ref} );
-
-    # Get possible executable to test
-    my $program_executable = $commands_ref->[0];
-
-    ## Check if executable has a singularity container
-    my $singularity_container;
-
-    if ( defined $SINGULARITY_CONTAINER{$program_executable} ) {
-
-        $singularity_container =
-          $SINGULARITY_CONTAINER{$program_executable}{container_path};
-    }
-
-    # Return if no singularity image was found
-    return if ( not $singularity_container );
-
-    ## Get bind paths
-    my @bind_paths = @SINGULARITY_BIND_PATHS;
-    if ( $SINGULARITY_CONTAINER{$program_executable}{extra_bind_paths} ) {
-
-        push @bind_paths,
-          @{ $SINGULARITY_CONTAINER{$program_executable}{extra_bind_paths} };
-    }
-
-    ## Remove potential undef values
-    @bind_paths = grep { defined } @bind_paths;
-
-    ## Remove overlapping paths
-    @bind_paths = parse_sing_bind_paths( { dir_paths_ref => \@bind_paths, } );
-
-    # Build complete command
-    @{$commands_ref} = singularity_exec(
-        {
-            bind_paths_ref                 => \@bind_paths,
-            singularity_container          => $singularity_container,
-            singularity_container_cmds_ref => $commands_ref,
-        }
-    );
-
-    return @{$commands_ref};
+    our @EXPORT_OK = qw{ parse_sing_bind_paths };
 }
 
 sub parse_sing_bind_paths {
