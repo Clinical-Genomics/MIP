602210378c5ed58a18ce7194b61258fa5108d4b4
henrikstranneheim <henrik.stranneheim@scilifelab.se>
2019-11-12 16:04:45 +0100

    feat(mip_bind): Added bcftools install recipe to add MIP_BIND
    - Updated CADD recipe to add MIP_BIND if not supplied with a reference dir
    - Updated tests

--- lib/MIP/Recipes/Install/Bcftools.pm
+++ lib/MIP/Recipes/Install/Bcftools.pm
@@ -1,4 +1,4 @@
-package MIP::Recipes::Install::Cadd;
+package MIP::Recipes::Install::Bcftools;
 
 use 5.026;
 use Carp;
@@ -20,7 +20,7 @@ use autodie qw{ :all };
 use Readonly;
 
 ## MIPs lib/
-use MIP::Constants qw{ $COLON $DOUBLE_QUOTE $FORWARD_SLASH $LOG_NAME $NEWLINE $SPACE };
+use MIP::Constants qw{ $DOUBLE_QUOTE };
 use MIP::Gnu::Coreutils qw{ gnu_mkdir };
 use MIP::Program::Singularity qw{ singularity_exec };
 
@@ -29,15 +29,15 @@ BEGIN {
     use base qw{ Exporter };
 
     # Set the version for version checking
-    our $VERSION = 1.04;
+    our $VERSION = 1.00;
 
     # Functions and variables which can be optionally exported
-    our @EXPORT_OK = qw{ install_cadd };
+    our @EXPORT_OK = qw{ install_bcftools };
 }
 
-sub install_cadd {
+sub install_bcftools {
 
-## Function : Install/download references and annotations for CADD
+## Function : Add relative bind path for bcftools
 ## Returns  :
 ## Arguments: $active_parameter_href => Active parameter hash {REF}
 ##          : $contaienr_href        => Container hah {REF}
@@ -79,34 +79,18 @@ sub install_cadd {
 
     check( $tmpl, $arg_href, 1 ) or croak q{Could not parse arguments!};
 
-    ## Retrieve logger object
-    my $log = Log::Log4perl->get_logger($LOG_NAME);
+    ## Only add MIP_BIND for bcftools
+    return if ( not $container_path =~ /bcftools.sif \z/sxm );
 
-    ## Unpack parameters
-    my $reference_dir_path = $active_parameter_href->{reference_dir};
-
-    if ( not $reference_dir_path ) {
-        $log->warn(
-q{Please supply a reference directory when installing CADD to use a static path}
-        );
-        $reference_dir_path = $DOUBLE_QUOTE . q{$MIP_BIND} . $DOUBLE_QUOTE;
-    }
-
-    my $annotation_dir_path =
-      catdir( $reference_dir_path, qw{ CADD-scripts data annotations } );
-
-    ## Bind annotation dir path to pathh in container
-    my $cadd_bind_path =
-        $annotation_dir_path
-      . $COLON
-      . catdir( $FORWARD_SLASH, qw{ opt CADD-scripts data annotations } );
+    my $bcftools_bind_path = $DOUBLE_QUOTE . q{$MIP_BIND} . $DOUBLE_QUOTE;
 
     ## Store annotation dir path for later
     if ( $container_href->{program_bind_paths} ) {
-        push @{ $container_href->{program_bind_paths} }, $cadd_bind_path;
+
+        push @{ $container_href->{program_bind_paths} }, $bcftools_bind_path;
     }
     else {
-        $container_href->{program_bind_paths} = [$cadd_bind_path];
+        $container_href->{program_bind_paths} = [$bcftools_bind_path];
     }
     return 1;
 }
