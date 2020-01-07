f736afc4759f39a26293a4d03312b7e2350b50ea
henrikstranneheim <henrik.stranneheim@scilifelab.se>
2020-01-07 14:20:42 +0100

    feat(set_cache): Order import AB

--- lib/MIP/Main/Download.pm
+++ lib/MIP/Main/Download.pm
@@ -30,8 +30,8 @@ use MIP::Constants
   qw{ $COLON $COMMA $DOT $MIP_VERSION $NEWLINE $SINGLE_QUOTE $SPACE $UNDERSCORE };
 use MIP::File::Format::Yaml qw{ load_yaml };
 use MIP::Log::MIP_log4perl qw{ get_log };
-use MIP::Parse::Parameter qw{ parse_download_reference_parameter };
 use MIP::Parameter qw{ set_cache };
+use MIP::Parse::Parameter qw{ parse_download_reference_parameter };
 use MIP::Recipes::Pipeline::Download_rd_dna qw{ pipeline_download_rd_dna };
 use MIP::Recipes::Pipeline::Download_rd_rna qw{ pipeline_download_rd_rna };
 use MIP::Set::Parameter
