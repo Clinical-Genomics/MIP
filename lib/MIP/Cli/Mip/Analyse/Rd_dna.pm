59f25e219c58c2c237df97103131b7067dd7d6f8
henrikstranneheim <henrik.stranneheim@scilifelab.se>
2020-01-13 14:57:39 +0100

    feat(cli and definition): Added recipe switch

--- lib/MIP/Cli/Mip/Analyse/Rd_dna.pm
+++ lib/MIP/Cli/Mip/Analyse/Rd_dna.pm
@@ -17,7 +17,7 @@ use Moose::Util::TypeConstraints;
 ## MIPs lib
 use MIP::Main::Analyse qw{ mip_analyse };
 
-our $VERSION = 1.40;
+our $VERSION = 1.41;
 
 extends(qw{ MIP::Cli::Mip::Analyse });
 
@@ -682,6 +682,16 @@ q{Default: grch37_dbsnp_-138-.vcf, grch37_1000g_indels_-phase1-.vcf, grch37_mill
             documentation => q{Do not include reads with failed quality control},
             is            => q{rw},
             isa           => Bool,
+        )
+    );
+
+    option(
+        q{smncopynumbercaller} => (
+            cmd_aliases   => [qw{ smncnc }],
+            cmd_tags      => [q{Analysis recipe switch}],
+            documentation => q{SMN copy number analysis},
+            is            => q{rw},
+            isa           => enum( [ 0, 1, 2 ] ),
         )
     );
 
