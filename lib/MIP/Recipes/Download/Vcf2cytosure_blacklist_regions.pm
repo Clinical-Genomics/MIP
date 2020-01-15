fa81fcb74aa805731abdadbde54751509fb0ae70
henrikstranneheim <henrik.stranneheim@scilifelab.se>
2020-01-15 15:57:30 +0100

    feat(download): Added download of blacklist to mip download rd_dna. WIP (still needs updates to analysis recipe)
    - Added test

--- lib/MIP/Recipes/Download/Vcf2cytosure_blacklist_regions.pm
+++ lib/MIP/Recipes/Download/Vcf2cytosure_blacklist_regions.pm
@@ -1,4 +1,4 @@
-package MIP::Recipes::Download::RECIPE_NAME;
+package MIP::Recipes::Download::Vcf2cytosure_blacklist_regions;
 
 use 5.026;
 use Carp;
@@ -28,13 +28,13 @@ BEGIN {
     our $VERSION = 1.00;
 
     # Functions and variables which can be optionally exported
-    our @EXPORT_OK = qw{ download_RECIPE_NAME };
+    our @EXPORT_OK = qw{ download_vcf2cytosure_blacklist_regions };
 
 }
 
-sub download_RECIPE_NAME {
+sub download_vcf2cytosure_blacklist_regions {
 
-## Function : Download RECIPE_NAME
+## Function : Download vcf2cytosure blacklist files
 ## Returns  :
 ## Arguments: $active_parameter_href => Active parameters for this download hash {REF}
 ##          : $genome_version        => Human genome version
