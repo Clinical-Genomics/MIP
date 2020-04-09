af6f74873c7a122972cbedc06ba7d5fb25c98890
jemten <jemten@users.noreply.github.com>
2020-04-09 14:25:37 +0200

    fixed typo

--- lib/MIP/Recipes/Install/Mip_scripts.pm
+++ lib/MIP/Recipes/Install/Mip_scripts.pm
@@ -131,7 +131,7 @@ sub install_mip_scripts {
               mip_rd_dna_vcf_rerun_config.yaml
               mip_rd_rna_config.yaml
               program_test_cmds.yaml
-              qc_eval_metric_v1.0.yaml
+              qc_eval_metric_-v1.0-.yaml
               qc_regexp_-v1.24-.yaml
               rank_model_cmms_-v1.28-.ini
               svrank_model_cmms_-v1.8-.ini
