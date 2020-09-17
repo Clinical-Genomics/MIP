f7ac80ce399b51a60a5d348a416c51e985ff174b
henrikstranneheim <henrik.stranneheim@scilifelab.se>
2020-09-17 12:12:57 +0200

    feat(refactor): Creates utils sub

--- lib/MIP/Recipes/Analysis/Smncopynumbercaller.pm
+++ lib/MIP/Recipes/Analysis/Smncopynumbercaller.pm
@@ -16,7 +16,8 @@ use autodie qw{ :all };
 use Readonly;
 
 ## MIPs lib/
-use MIP::Constants qw{ $GENOME_VERSION $LOG_NAME $NEWLINE $SINGLE_QUOTE $SPACE $UNDERSCORE };
+use MIP::Constants
+  qw{ $GENOME_VERSION $LOG_NAME $NEWLINE $SINGLE_QUOTE $SPACE $UNDERSCORE };
 
 BEGIN {
 
@@ -33,17 +34,17 @@ BEGIN {
 
 sub analysis_smncopynumbercaller {
 
-    ## Function : Call the copy number of full-length SMN1, full-length SMN2,
-    ##          : as well as SMN2Δ7–8 (SMN2 with a deletion of Exon7-8).
-    ## Returns  :
-    ## Arguments: $active_parameter_href   => Active parameters for this analysis hash {REF}
-    ##          : $case_id                 => Family id
-    ##          : $file_info_href          => File_info hash {REF}
-    ##          : $job_id_href             => Job id hash {REF}
-    ##          : $parameter_href          => Parameter hash {REF}
-    ##          : $profile_base_command    => Submission profile base command
-    ##          : $recipe_name             => Recipe name
-    ##          : $sample_info_href        => Info on samples and case hash {REF}
+## Function : Call the copy number of full-length SMN1, full-length SMN2,
+##          : as well as SMN2Δ7–8 (SMN2 with a deletion of Exon7-8).
+## Returns  :
+## Arguments: $active_parameter_href   => Active parameters for this analysis hash {REF}
+##          : $case_id                 => Family id
+##          : $file_info_href          => File_info hash {REF}
+##          : $job_id_href             => Job id hash {REF}
+##          : $parameter_href          => Parameter hash {REF}
+##          : $profile_base_command    => Submission profile base command
+##          : $recipe_name             => Recipe name
+##          : $sample_info_href        => Info on samples and case hash {REF}
 
     my ($arg_href) = @_;
 
@@ -67,44 +68,44 @@ sub analysis_smncopynumbercaller {
             store       => \$active_parameter_href,
             strict_type => 1,
         },
-        case_id               => {
+        case_id => {
             default     => $arg_href->{active_parameter_href}{case_id},
             store       => \$case_id,
             strict_type => 1,
         },
-        file_info_href        => {
+        file_info_href => {
             default     => {},
             defined     => 1,
             required    => 1,
             store       => \$file_info_href,
             strict_type => 1,
         },
-        job_id_href           => {
+        job_id_href => {
             default     => {},
             defined     => 1,
             required    => 1,
             store       => \$job_id_href,
             strict_type => 1,
         },
-        parameter_href        => {
+        parameter_href => {
             default     => {},
             defined     => 1,
             required    => 1,
             store       => \$parameter_href,
             strict_type => 1,
         },
-        profile_base_command  => {
+        profile_base_command => {
             default     => q{sbatch},
             store       => \$profile_base_command,
             strict_type => 1,
         },
-        recipe_name           => {
+        recipe_name => {
             defined     => 1,
             required    => 1,
             store       => \$recipe_name,
             strict_type => 1,
         },
-        sample_info_href      => {
+        sample_info_href => {
             default     => {},
             defined     => 1,
             required    => 1,
@@ -113,7 +114,7 @@ sub analysis_smncopynumbercaller {
         },
     };
 
-    check($tmpl, $arg_href, 1) or croak q{Could not parse arguments!};
+    check( $tmpl, $arg_href, 1 ) or croak q{Could not parse arguments!};
 
     use MIP::Get::File qw{get_io_files};
     use MIP::Get::Parameter qw{get_recipe_attributes get_recipe_resources};
@@ -138,7 +139,7 @@ sub analysis_smncopynumbercaller {
             recipe_name    => $recipe_name,
         }
     );
-    my $recipe_mode = $active_parameter_href->{$recipe_name};
+    my $recipe_mode     = $active_parameter_href->{$recipe_name};
     my %recipe_resource = get_recipe_resources(
         {
             active_parameter_href => $active_parameter_href,
@@ -148,8 +149,8 @@ sub analysis_smncopynumbercaller {
 
     my @infile_paths;
     my %sample_file_prefix;
-    SAMPLE_ID:
-    foreach my $sample_id (@{$active_parameter_href->{sample_ids}}) {
+  SAMPLE_ID:
+    foreach my $sample_id ( @{ $active_parameter_href->{sample_ids} } ) {
 
         ## Get the io infiles per chain and id
         my %sample_io = get_io_files(
@@ -163,7 +164,7 @@ sub analysis_smncopynumbercaller {
         );
         my $file_name_prefix = $sample_io{in}{file_name_prefix};
         my $file_path_prefix = $sample_io{in}{file_path_prefix};
-        my $file_suffix = $sample_io{in}{file_suffix};
+        my $file_suffix      = $sample_io{in}{file_suffix};
         push @infile_paths, $file_path_prefix . $file_suffix;
         $sample_file_prefix{$sample_id} = $file_name_prefix;
     }
@@ -173,22 +174,22 @@ sub analysis_smncopynumbercaller {
             chain_id               => $job_id_chain,
             id                     => $case_id,
             file_info_href         => $file_info_href,
-            file_name_prefixes_ref => [ $case_id ],
+            file_name_prefixes_ref => [$case_id],
             outdata_dir            => $active_parameter_href->{outdata_dir},
             parameter_href         => $parameter_href,
             recipe_name            => $recipe_name,
         }
     );
-    my $outdir_path_prefix = $io{out}{dir_path_prefix};
+    my $outdir_path_prefix  = $io{out}{dir_path_prefix};
     my $outfile_name_prefix = $io{out}{file_name_prefix};
-    my $outfile_path = $io{out}{file_path};
+    my $outfile_path        = $io{out}{file_path};
 
     ## Filehandles
     # Create anonymous filehandle
     my $filehandle = IO::Handle->new();
 
     ## Creates recipe directories (info & data & script), recipe script filenames and writes sbatch header
-    my ($recipe_file_path, $recipe_info_path) = setup_script(
+    my ( $recipe_file_path, $recipe_info_path ) = setup_script(
         {
             active_parameter_href           => $active_parameter_href,
             core_number                     => $recipe_resource{core_number},
@@ -208,7 +209,7 @@ sub analysis_smncopynumbercaller {
     say {$filehandle} q{## } . $recipe_name;
 
     ## Create manifest file
-    my $manifest_file_path = catfile($outdir_path_prefix, q{manifest.txt});
+    my $manifest_file_path = catfile( $outdir_path_prefix, q{manifest.txt} );
 
     gnu_echo(
         {
@@ -233,21 +234,15 @@ sub analysis_smncopynumbercaller {
     );
     say {$filehandle} $NEWLINE;
 
-    ## Rename output from $file_path_prefixes to $sample_id
-    while (my ($sample_id, $file_name_prefix) = each %sample_file_prefix) {
-
-    my @perl_commands = perl_base(
+    _use_sample_id_in_output(
         {
-            command_line => 1,
-            inplace      => 1,
-            print        => 1,
+            filehandle              => $filehandle,
+            outfile_path            => $outfile_path,
+            sample_file_prefix_href => \%sample_file_prefix,
         }
     );
-    push @perl_commands, ($SINGLE_QUOTE, qq{s/$file_name_prefix/$sample_id/gc}, $SINGLE_QUOTE, $outfile_path);
-        say {$filehandle} join $SPACE, @perl_commands;
-    }
 
-    ## Close filehandleS
+    ## Close filehandle
     close $filehandle or $log->logcroak(q{Could not close filehandle});
 
     if ( $recipe_mode == 1 ) {
@@ -289,6 +284,62 @@ sub analysis_smncopynumbercaller {
         );
     }
     return 1;
+}
+
+sub _use_sample_id_in_output {
+
+## Function : Rename file_name_prefix to sample_id for sample column
+## Returns  :
+## Arguments: $filehandle              => Filehandle to write to
+##          : $outfile_path            => Outfile path to use for search and replace
+##          : $sample_file_prefix_href => Map of file_name_prefix and sample_id
+
+    my ($arg_href) = @_;
+
+    ## Flatten argument(s)
+    my $filehandle;
+    my $outfile_path;
+    my $sample_file_prefix_href;
+
+    my $tmpl = {
+        filehandle => {
+            required => 1,
+            store    => \$filehandle,
+        },
+        outfile_path => {
+            defined     => 1,
+            required    => 1,
+            store       => \$outfile_path,
+            strict_type => 1,
+        },
+        sample_file_prefix_href => {
+            default     => {},
+            defined     => 1,
+            required    => 1,
+            store       => \$sample_file_prefix_href,
+            strict_type => 1,
+        },
+    };
+
+    check( $tmpl, $arg_href, 1 ) or croak q{Could not parse arguments!};
+
+    while ( my ( $sample_id, $file_name_prefix ) = each %{$sample_file_prefix_href} ) {
+
+        my @perl_commands = perl_base(
+            {
+                command_line => 1,
+                inplace      => 1,
+                print        => 1,
+            }
+        );
+        push @perl_commands,
+          (
+            $SINGLE_QUOTE, qq{s/$file_name_prefix/$sample_id/gc},
+            $SINGLE_QUOTE, $outfile_path
+          );
+        say {$filehandle} join $SPACE, @perl_commands;
+    }
+    return;
 }
 
 1;
