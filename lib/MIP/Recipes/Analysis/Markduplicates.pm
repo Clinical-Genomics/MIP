56d2d28452d01d5430fe218ec46303f36d845b9f
Ramprasad Neethiraj <20065894+ramprasadn@users.noreply.github.com>
2020-09-25 15:20:48 +0200

    Update version number

--- lib/MIP/Recipes/Analysis/Markduplicates.pm
+++ lib/MIP/Recipes/Analysis/Markduplicates.pm
@@ -27,7 +27,7 @@ BEGIN {
     use base qw{ Exporter };
 
     # Set the version for version checking
-    our $VERSION = 1.28;
+    our $VERSION = 1.29;
 
     # Functions and variables which can be optionally exported
     our @EXPORT_OK =
