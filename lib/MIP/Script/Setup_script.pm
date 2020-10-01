4d95f7063018cb60a354f999b34135a3ce072c11
henrikstranneheim <henrik.stranneheim@scilifelab.se>
2020-10-01 13:14:11 +0200

    feat(review): Fix comment

--- lib/MIP/Script/Setup_script.pm
+++ lib/MIP/Script/Setup_script.pm
@@ -45,71 +45,6 @@ BEGIN {
     };
 }
 
-sub _broadcast_script_paths {
-
-## Function : Broadcast script and data directory paths
-## Returns  :
-## Arguments: $file_path                  => Script file path
-##          : $recipe_data_directory_path => Recipe data file output directory
-##          : $recipe_name                => Name of recipe
-##          : $script_type                => Dry run or sharp
-
-    my ($arg_href) = @_;
-
-    ## Flatten argument(s)
-    my $file_path;
-    my $recipe_data_directory_path;
-    my $recipe_name;
-    my $script_type;
-
-    my $tmpl = {
-        file_path => {
-            defined     => 1,
-            required    => 1,
-            store       => \$file_path,
-            strict_type => 1,
-        },
-        recipe_data_directory_path => {
-            defined     => 1,
-            required    => 1,
-            store       => \$recipe_data_directory_path,
-            strict_type => 1,
-        },
-        recipe_name => {
-            defined     => 1,
-            required    => 1,
-            store       => \$recipe_name,
-            strict_type => 1,
-        },
-        script_type => {
-            defined     => 1,
-            required    => 1,
-            store       => \$script_type,
-            strict_type => 1,
-        },
-    };
-
-    check( $tmpl, $arg_href, 1 ) or croak q{Could not parse arguments!};
-
-    my $log = Log::Log4perl->get_logger($LOG_NAME);
-
-    $log->info( q{Creating }
-          . $script_type
-          . q{ script for }
-          . $recipe_name
-          . q{ and writing script file(s) to: }
-          . $file_path
-          . $NEWLINE );
-    $log->info(
-            ucfirst $script_type
-          . q{ script }
-          . $recipe_name
-          . q{ data files will be written to: }
-          . $recipe_data_directory_path
-          . $NEWLINE );
-    return;
-}
-
 sub build_script_directories_and_paths {
 
 ## Function : Builds and makes recipe directories (info & data & script) and recipe script paths
@@ -1147,6 +1082,71 @@ sub write_return_to_conda_environment {
         );
         print {$filehandle} $NEWLINE;
     }
+    return;
+}
+
+sub _broadcast_script_paths {
+
+## Function : Broadcast script and data directory paths
+## Returns  :
+## Arguments: $file_path                  => Script file path
+##          : $recipe_data_directory_path => Recipe data file output directory
+##          : $recipe_name                => Name of recipe
+##          : $script_type                => Dry run or sharp
+
+    my ($arg_href) = @_;
+
+    ## Flatten argument(s)
+    my $file_path;
+    my $recipe_data_directory_path;
+    my $recipe_name;
+    my $script_type;
+
+    my $tmpl = {
+        file_path => {
+            defined     => 1,
+            required    => 1,
+            store       => \$file_path,
+            strict_type => 1,
+        },
+        recipe_data_directory_path => {
+            defined     => 1,
+            required    => 1,
+            store       => \$recipe_data_directory_path,
+            strict_type => 1,
+        },
+        recipe_name => {
+            defined     => 1,
+            required    => 1,
+            store       => \$recipe_name,
+            strict_type => 1,
+        },
+        script_type => {
+            defined     => 1,
+            required    => 1,
+            store       => \$script_type,
+            strict_type => 1,
+        },
+    };
+
+    check( $tmpl, $arg_href, 1 ) or croak q{Could not parse arguments!};
+
+    my $log = Log::Log4perl->get_logger($LOG_NAME);
+
+    $log->info( q{Creating }
+          . $script_type
+          . q{ script for }
+          . $recipe_name
+          . q{ and writing script file(s) to: }
+          . $file_path
+          . $NEWLINE );
+    $log->info(
+            ucfirst $script_type
+          . q{ script }
+          . $recipe_name
+          . q{ data files will be written to: }
+          . $recipe_data_directory_path
+          . $NEWLINE );
     return;
 }
 
