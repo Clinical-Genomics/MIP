4dcc6f08f46733d6ccaf85169d51a2670c8e311e
henrikstranneheim <henrik.stranneheim@scilifelab.se>
2020-11-06 09:05:18 +0100

    feat(template): Removed sourcing from templates

--- templates/code/recipe_analysis_case.pm
+++ templates/code/recipe_analysis_case.pm
@@ -196,7 +196,6 @@ sub analysis_RECIPE_NAME {
             process_time                    => $recipe_resource{time},
             recipe_directory                => $recipe_name,
             recipe_name                     => $recipe_name,
-            source_environment_commands_ref => $recipe_resource{load_env_ref},
         }
     );
 
