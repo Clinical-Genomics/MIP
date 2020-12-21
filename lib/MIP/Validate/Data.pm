8543849a795a1c1c9c4bfbbd774ebbca4e4f216b
henrikstranneheim <henrik.stranneheim@scilifelab.se>
2020-12-21 11:13:00 +0100

    feat(constraint):

--- lib/MIP/Validate/Data.pm
+++ lib/MIP/Validate/Data.pm
@@ -25,7 +25,6 @@ BEGIN {
 our %constraint = (
     file_exists       => sub { return 1 if ( -e $_[0] );                  return; },
     dir_exists        => sub { return 1 if ( -d $_[0] );                  return; },
-    is_defined        => sub { return 1 if ( defined $_[0] );             return; },
     is_digit          => sub { return 1 if ( $_[0] =~ / \A \d+ \z /sxm ); return; },
     is_gzipped        => sub { return 1 if ( $_[0] =~ / [.]gz$ /xms );    return 0; },
     plain_file_exists => sub { return 1 if ( -f $_[0] );                  return; },
