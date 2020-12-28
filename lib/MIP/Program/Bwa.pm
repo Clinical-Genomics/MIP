73132fe3cb04bccf44ae56fa0b748772dfb4cd0d
jemten <jemten@users.noreply.github.com>
2020-12-28 17:29:48 +0100

    feat(bwa_mem): deterministic alignment

--- lib/MIP/Program/Bwa.pm
+++ lib/MIP/Program/Bwa.pm
@@ -27,7 +27,7 @@ BEGIN {
     our @EXPORT_OK = qw{ bwa_index bwa_mem bwa_mem2_mem bwa_mem2_index run_bwamem };
 
 }
-
+Readonly my $BASES_TO_PROCESS    => q{100000000};
 Readonly my $BWA_BASE_COMMAND    => q{bwa};
 Readonly my $BWA2_BASE_COMMAND   => q{bwa-mem2};
 Readonly my $RUNBWA_BASE_COMMAND => q{run-bwamem};
@@ -66,7 +66,7 @@ sub bwa_index {
         filehandle => {
             store => \$filehandle,
         },
-        prefix => { defined => 1, required => 1, store => \$prefix, strict_type => 1, },
+        prefix           => { defined => 1, required => 1, store => \$prefix, strict_type => 1, },
         reference_genome => {
             defined     => 1,
             required    => 1,
@@ -89,10 +89,8 @@ sub bwa_index {
 
     check( $tmpl, $arg_href, 1 ) or croak q{Could not parse arguments!};
 
-    my @commands = (
-        get_executable_base_command( { base_command => $BWA_BASE_COMMAND, } ),
-        qw{ index }
-    );
+    my @commands =
+      ( get_executable_base_command( { base_command => $BWA_BASE_COMMAND, } ), qw{ index } );
 
     push @commands, q{-p} . $SPACE . $prefix;
 
@@ -127,7 +125,8 @@ sub bwa_mem {
 
 ## Function : Perl wrapper for writing bwa mem recipe to $filehandle. Based on bwa 0.7.15-r1140.
 ## Returns  : @commands
-## Arguments: $filehandle              => Sbatch filehandle to write to
+## Arguments: $deterministic_alignment => Fixed number of input bases
+##          : $filehandle              => Sbatch filehandle to write to
 ##          : $idxbase                 => Idxbase (human genome references and bwa mem idx files)
 ##          : $infile_path             => Infile path (read 1 or interleaved i.e. read 1 and 2)
 ##          : $interleaved_fastq_file  => Smart pairing
@@ -155,10 +154,17 @@ sub bwa_mem {
     my $thread_number;
 
     ## Default(s)
+    my $deterministic_alignment;
     my $interleaved_fastq_file;
     my $mark_split_as_secondary;
 
     my $tmpl = {
+        deterministic_alignment => {
+            allow       => [ undef, 0, 1 ],
+            default     => 1,
+            store       => \$deterministic_alignment,
+            strict_type => 1,
+        },
         filehandle => { store => \$filehandle, },
         idxbase    => {
             defined     => 1,
@@ -188,15 +194,14 @@ sub bwa_mem {
         second_infile_path  => { store => \$second_infile_path, strict_type => 1, },
         soft_clip_sup_align => {
             allow       => [ undef, 0, 1 ],
-            default     => 0,
+            default     => 1,
             store       => \$soft_clip_sup_align,
             strict_type => 1,
         },
-        stderrfile_path => { store => \$stderrfile_path, strict_type => 1, },
-        stderrfile_path_append =>
-          { store => \$stderrfile_path_append, strict_type => 1, },
-        stdoutfile_path => { store => \$stdoutfile_path, strict_type => 1, },
-        thread_number   => {
+        stderrfile_path        => { store => \$stderrfile_path,        strict_type => 1, },
+        stderrfile_path_append => { store => \$stderrfile_path_append, strict_type => 1, },
+        stdoutfile_path        => { store => \$stdoutfile_path,        strict_type => 1, },
+        thread_number          => {
             allow       => qr/ ^\d+$ /xms,
             store       => \$thread_number,
             strict_type => 1,
@@ -206,9 +211,12 @@ sub bwa_mem {
     check( $tmpl, $arg_href, 1 ) or croak q{Could not parse arguments!};
 
     my @commands =
-      ( get_executable_base_command( { base_command => $BWA_BASE_COMMAND, } ),
-        qw{ mem } );
+      ( get_executable_base_command( { base_command => $BWA_BASE_COMMAND, } ), qw{ mem } );
 
+    if ($deterministic_alignment) {
+
+        push @commands, q{-K} . $SPACE . $BASES_TO_PROCESS;
+    }
     if ($thread_number) {
 
         push @commands, q{-t} . $SPACE . $thread_number;
@@ -265,7 +273,8 @@ sub bwa_mem2_mem {
 
 ## Function : Perl wrapper for writing bwa mem 2 mem recipe to $filehandle. Based on bwa mem 2 2.0.
 ## Returns  : @commands
-## Arguments: $filehandle              => Sbatch filehandle to write to
+## Arguments: $deterministic_alignment => Fixed number of input bases
+##          : $filehandle              => Sbatch filehandle to write to
 ##          : $idxbase                 => Idxbase (human genome references and bwa mem idx files)
 ##          : $infile_path             => Infile path (read 1 or interleaved i.e. read 1 and 2)
 ##          : $interleaved_fastq_file  => Smart pairing
@@ -293,10 +302,17 @@ sub bwa_mem2_mem {
     my $thread_number;
 
     ## Default(s)
+    my $deterministic_alignment;
     my $interleaved_fastq_file;
     my $mark_split_as_secondary;
 
     my $tmpl = {
+        deterministic_alignment => {
+            allow       => [ undef, 0, 1 ],
+            default     => 1,
+            store       => \$deterministic_alignment,
+            strict_type => 1,
+        },
         filehandle => { store => \$filehandle, },
         idxbase    => {
             defined     => 1,
@@ -330,11 +346,10 @@ sub bwa_mem2_mem {
             store       => \$soft_clip_sup_align,
             strict_type => 1,
         },
-        stderrfile_path => { store => \$stderrfile_path, strict_type => 1, },
-        stderrfile_path_append =>
-          { store => \$stderrfile_path_append, strict_type => 1, },
-        stdoutfile_path => { store => \$stdoutfile_path, strict_type => 1, },
-        thread_number   => {
+        stderrfile_path        => { store => \$stderrfile_path,        strict_type => 1, },
+        stderrfile_path_append => { store => \$stderrfile_path_append, strict_type => 1, },
+        stdoutfile_path        => { store => \$stdoutfile_path,        strict_type => 1, },
+        thread_number          => {
             allow       => qr/ ^\d+$ /xms,
             store       => \$thread_number,
             strict_type => 1,
@@ -343,11 +358,13 @@ sub bwa_mem2_mem {
 
     check( $tmpl, $arg_href, 1 ) or croak q{Could not parse arguments!};
 
-    my @commands = (
-        get_executable_base_command( { base_command => $BWA2_BASE_COMMAND, } ),
-        qw{ mem }
-    );
+    my @commands =
+      ( get_executable_base_command( { base_command => $BWA2_BASE_COMMAND, } ), qw{ mem } );
 
+    if ($deterministic_alignment) {
+
+        push @commands, q{-K} . $SPACE . $BASES_TO_PROCESS;
+    }
     if ($thread_number) {
 
         push @commands, q{-t} . $SPACE . $thread_number;
@@ -427,7 +444,7 @@ sub bwa_mem2_index {
         filehandle => {
             store => \$filehandle,
         },
-        prefix => { defined => 1, required => 1, store => \$prefix, strict_type => 1, },
+        prefix           => { defined => 1, required => 1, store => \$prefix, strict_type => 1, },
         reference_genome => {
             defined     => 1,
             required    => 1,
@@ -450,10 +467,8 @@ sub bwa_mem2_index {
 
     check( $tmpl, $arg_href, 1 ) or croak q{Could not parse arguments!};
 
-    my @commands = (
-        get_executable_base_command( { base_command => $BWA2_BASE_COMMAND, } ),
-        qw{ index }
-    );
+    my @commands =
+      ( get_executable_base_command( { base_command => $BWA2_BASE_COMMAND, } ), qw{ index } );
 
     push @commands, q{-p} . $SPACE . $prefix;
 
@@ -535,12 +550,11 @@ sub run_bwamem {
             store       => \$outfiles_prefix_path,
             strict_type => 1,
         },
-        read_group_header  => { store => \$read_group_header,  strict_type => 1, },
-        second_infile_path => { store => \$second_infile_path, strict_type => 1, },
-        stderrfile_path    => { store => \$stderrfile_path,    strict_type => 1, },
-        stderrfile_path_append =>
-          { store => \$stderrfile_path_append, strict_type => 1, },
-        thread_number => {
+        read_group_header      => { store => \$read_group_header,      strict_type => 1, },
+        second_infile_path     => { store => \$second_infile_path,     strict_type => 1, },
+        stderrfile_path        => { store => \$stderrfile_path,        strict_type => 1, },
+        stderrfile_path_append => { store => \$stderrfile_path_append, strict_type => 1, },
+        thread_number          => {
             allow       => qr/ ^\d+$ /xms,
             store       => \$thread_number,
             strict_type => 1,
