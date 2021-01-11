76a8c5adf384902dfd94882cf1e1ef3bf86f8e43
henrikstranneheim <henrik.stranneheim@scilifelab.se>
2021-01-11 18:53:23 +0100

    feat(recipe): Added also for deeptrio recipe
    - Fix template

--- lib/MIP/Recipes/Analysis/Deeptrio.pm
+++ lib/MIP/Recipes/Analysis/Deeptrio.pm
@@ -108,11 +108,11 @@ sub analysis_deeptrio {
     check( $tmpl, $arg_href, 1 ) or croak q{Could not parse arguments!};
 
     use MIP::Get::File qw{ get_io_files };
-    use MIP::Get::Parameter qw{ get_recipe_attributes get_recipe_resources };
     use MIP::Parse::File qw{ parse_io_outfiles };
     use MIP::Processmanagement::Processes qw{ submit_recipe };
     use MIP::Program::Deeptrio qw{ deeptrio };
     use MIP::Program::Gnu::Coreutils qw{ gnu_mkdir };
+    use MIP::Recipe qw{ parse_recipe_prerequisites };
     use MIP::Sample_info
       qw{ get_case_members_attributes_in_duos get_family_member_id set_recipe_metafile_in_sample_info set_recipe_outfile_in_sample_info };
     use MIP::Script::Setup_script qw{ setup_script };
@@ -128,25 +128,17 @@ sub analysis_deeptrio {
       : get_family_member_id( { sample_info_href => $sample_info_href } );
 
     ## Unpack parameters
-    my $job_id_chain = get_recipe_attributes(
+    my %recipe = parse_recipe_prerequisites(
         {
-            attribute      => q{chain},
-            parameter_href => $parameter_href,
-            recipe_name    => $recipe_name,
+            active_parameter_href => $active_parameter_href,
+            parameter_href        => $parameter_href,
+            recipe_name           => $recipe_name,
         }
     );
 
     my $model_type = _consensus_analysis_type(
         {
             parameter_href => $parameter_href,
-        }
-    );
-
-    my $recipe_mode     = $active_parameter_href->{$recipe_name};
-    my %recipe_resource = get_recipe_resources(
-        {
-            active_parameter_href => $active_parameter_href,
-            recipe_name           => $recipe_name,
         }
     );
 
@@ -176,7 +168,7 @@ sub analysis_deeptrio {
             %sample_bam_io,
             parse_io_outfiles(
                 {
-                    chain_id               => $job_id_chain,
+                    chain_id               => $recipe{job_id_chain},
                     id                     => $sample_id,
                     file_info_href         => $file_info_href,
                     file_name_prefixes_ref => [$infile_name_prefix],
@@ -203,13 +195,13 @@ sub analysis_deeptrio {
     my ( $recipe_file_path, $recipe_info_path ) = setup_script(
         {
             active_parameter_href => $active_parameter_href,
-            core_number           => $recipe_resource{core_number},
+            core_number           => $recipe{core_number},
             directory_id          => $case_id,
             filehandle            => $filehandle,
-            gpu_number            => $recipe_resource{gpu_number},
+            gpu_number            => $recipe{gpu_number},
             job_id_href           => $job_id_href,
-            memory_allocation     => $recipe_resource{memory},
-            process_time          => $recipe_resource{time},
+            memory_allocation     => $recipe{memory},
+            process_time          => $recipe{time},
             recipe_directory      => $recipe_name,
             recipe_name           => $recipe_name,
         }
@@ -234,7 +226,7 @@ sub analysis_deeptrio {
         {
             filehandle          => $filehandle,
             model_type          => $model_type,
-            num_shards          => $recipe_resource{core_number},
+            num_shards          => $recipe{core_number},
             output_gvcf_child   => $iofile_parameter{$child_id}{output_gvcf},
             output_gvcf_parent1 => $iofile_parameter{ $parents[0] }{output_gvcf},
             output_gvcf_parent2 => $iofile_parameter{ $parents[1] }{output_gvcf},
@@ -254,7 +246,7 @@ sub analysis_deeptrio {
     ## Close filehandleS
     close $filehandle or $log->logcroak(q{Could not close filehandle});
 
-    if ( $recipe_mode == 1 ) {
+    if ( $recipe{mode} == 1 ) {
 
         ## Collect QC metadata info for later use
         set_recipe_outfile_in_sample_info(
@@ -270,7 +262,7 @@ sub analysis_deeptrio {
                 base_command                      => $profile_base_command,
                 case_id                           => $case_id,
                 dependency_method                 => q{sample_to_case},
-                job_id_chain                      => $job_id_chain,
+                job_id_chain                      => $recipe{job_id_chain},
                 job_id_href                       => $job_id_href,
                 job_reservation_name              => $active_parameter_href->{job_reservation_name},
                 log                               => $log,
