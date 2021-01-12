842dd3603ab317a9876bc6f6cbd6e126c67188de
henrikstranneheim <henrik.stranneheim@scilifelab.se>
2021-01-12 13:56:02 +0100

    fix(import):

--- lib/MIP/Recipes/Pipeline/Analyse_rd_dna.pm
+++ lib/MIP/Recipes/Pipeline/Analyse_rd_dna.pm
@@ -438,7 +438,7 @@ sub pipeline_analyse_rd_dna {
     use MIP::Log::MIP_log4perl qw{ log_display_recipe_for_user };
     use MIP::Parse::Reference qw{ parse_references };
     use MIP::Set::Analysis
-      qw{ set_recipe_bwa_mem set_recipe_gatk_variantrecalibration set_recipe_on_analysis_type };
+      qw{ set_recipe_bwa_mem set_recipe_deepvariant set_recipe_gatk_variantrecalibration set_recipe_on_analysis_type };
 
     ## Recipes
     use MIP::Recipes::Analysis::Analysisrunstatus qw{ analysis_analysisrunstatus };
