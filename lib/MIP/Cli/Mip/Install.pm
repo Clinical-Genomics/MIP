8ad180cbf8f8998ac3f9cecbd86bb517d730b5c6
jemten <jemten@users.noreply.github.com>
2021-02-22 18:07:51 +0100

    feat(megafusion): installation

--- lib/MIP/Cli/Mip/Install.pm
+++ lib/MIP/Cli/Mip/Install.pm
@@ -131,10 +131,10 @@ sub _build_usage {
                 enum(
                     [
                         qw{ arriba bedtools blobfish bootstrapann bwa bwakit bwa-mem2 cadd chanjo
-                          chromograph cnvnator cyrius deeptrio deepvariant delly expansionhunter fastqc gatk 
-                          gatk4 genmod gffcompare glnexus htslib manta mip mip_scripts multiqc peddy picard plink
-                          preseq python rhocall rseqc rtg-tools salmon sambamba smncopynumbercaller star
-                          star-fusion stranger stringtie svdb telomerecat tiddit trim-galore ucsc upd
+                          chromograph cnvnator cyrius deeptrio deepvariant delly expansionhunter fastqc gatk
+                          gatk4 genmod gffcompare glnexus htslib manta megafusion mip mip_scripts multiqc peddy
+                          picard plink preseq python rhocall rseqc rtg-tools salmon sambamba smncopynumbercaller
+                          star star-fusion stranger stringtie svdb telomerecat tiddit trim-galore ucsc upd
                           utilities varg vcf2cytosure vcfanno vep vts }
                     ]
                 ),
@@ -153,10 +153,10 @@ sub _build_usage {
                 enum(
                     [
                         qw{ arriba bedtools blobfish bootstrapann bwa bwakit bwa-mem2 cadd chanjo
-                          chromograph cnvnator cyrius deeptrio deepvariant delly expansionhunter fastqc gatk 
-                          gatk4 genmod gffcompare glnexus htslib manta mip mip_scripts multiqc peddy picard plink
-                          preseq python rhocall rseqc rtg-tools salmon sambamba smncopynumbercaller star
-                          star-fusion stranger stringtie svdb telomerecat tiddit trim-galore ucsc upd
+                          chromograph cnvnator cyrius deeptrio deepvariant delly expansionhunter fastqc gatk
+                          gatk4 genmod gffcompare glnexus htslib manta megafusion mip mip_scripts multiqc peddy
+                          picard plink preseq python rhocall rseqc rtg-tools salmon sambamba smncopynumbercaller
+                          star star-fusion stranger stringtie svdb telomerecat tiddit trim-galore ucsc upd
                           utilities varg vcf2cytosure vcfanno vep vts }
                     ]
                 ),
