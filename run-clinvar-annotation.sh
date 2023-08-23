593cd28695584f03715a23940ceaec2d0e3c6b3c
Tor Björgen <tor.bjorgen@scilifelab.se>
2023-08-23 16:04:58 +0200

    Update runscript to use clinvar data

--- run-clinvar-annotation.sh
+++ run-clinvar-annotation.sh
@@ -1,1 +1,4 @@
-perl mip analyse rd_dna_vcf_rerun anderstestdata --container_config /home/proj/stage/bin/miniconda3/envs/D_mip_torb/bin/templates/mip_container_config.yaml -c templates/clinvar-config.yaml --vcf_rerun_file cases/anderstestdata/pathogenic_fixed.vcf.gz --sv_vcf_rerun_file cases/anderstestdata/pathogenic_fixed.vcf.gz
+# . init-cg-conda
+# us
+# conda activate D_mip_torb
+perl mip analyse rd_dna_vcf_rerun clinvar --container_config /home/proj/stage/bin/miniconda3/envs/D_mip_torb/bin/templates/mip_container_config.yaml -c templates/clinvar-config.yaml --vcf_rerun_file cases/clinvar/clinvar_20230617-cleaned.vcf.gz --sv_vcf_rerun_file cases/clinvar/clinvar_20230617-cleaned.vcf.gz
