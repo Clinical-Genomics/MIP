d985223c7f29f91c1a52857f3d8000e6afa94073
jemten <jemten@users.noreply.github.com>
2024-02-03 16:29:31 +0100

    adding command module

---
+++ lib/MIP/Program/Fastp.pm
@@ -0,0 +1,224 @@
+package MIP::Program::Fastp;
+
+use 5.026;
+use Carp;
+use charnames qw{ :full :short };
+use English qw{ -no_match_vars };
+use open qw{ :encoding(UTF-8) :std };
+use Params::Check qw{ allow check last_error };
+use utf8;
+use warnings;
+use warnings qw{ FATAL utf8 };
+
+## CPANM
+use autodie qw{ :all };
+use Readonly;
+
+## MIPs lib/
+use MIP::Constants qw{ $SPACE };
+use MIP::Environment::Executable qw{ get_executable_base_command };
+use MIP::Unix::Standard_streams qw{ unix_standard_streams };
+use MIP::Unix::Write_to_file qw{ unix_write_to_file };
+
+BEGIN {
+    require Exporter;
+    use base qw{ Exporter };
+
+    # Functions and variables which can be optionally exported
+    our @EXPORT_OK = qw{ fastp };
+}
+
+Readonly my $BASE_COMMAND => q{fastp};
+
+sub fastp {
+
+## Function : Perl wrapper for Fastp version 0.23.4
+## Returns  : @commands
+## Arguments: $detect_pe_adapter           => Detect adapter for parired end reads
+##          : $dont_eval_duplication       => Don't evaluate duplication rate to saves time and memory
+##          : $filehandle                  => Filehandle to write to
+##          : $first_infile_path           => Path to input read 1 fastq
+##          : $first_outfile_path          => Path to output read 1 fastq
+##          : $interleaved_in              => Interleaved fastq file
+##          : $length_required             => Minimum required length after trimming
+##          : $low_complexity_filter       => Filter low complexity regions
+##          : $overrepresentation_analysis => Enable overrepresentation analysis
+##          : $second_infile_path          => Path to input read 2 fastq
+##          : $second_outfile_path         => Path to output read 2 fastq
+##          : $stderrfile_path             => Stderrfile path
+##          : $stderrfile_path_append      => Append stderr info to file path
+##          : $stdoutfile_path             => Stdoutfile path
+##          : $threads                     => Number of threads
+##          : $trim_poly_g                 => Force poly g trimming
+
+    my ($arg_href) = @_;
+
+    ## Flatten argument(s)
+    my $detect_pe_adapter;
+    my $dont_eval_duplication;
+    my $filehandle;
+    my $first_infile_path;
+    my $first_outfile_path;
+    my $interleaved_in;
+    my $length_required;
+    my $low_complexity_filter;
+    my $overrepresentation_analysis;
+    my $second_infile_path;
+    my $second_outfile_path;
+    my $stderrfile_path;
+    my $stderrfile_path_append;
+    my $stdoutfile_path;
+    my $threads;
+    my $trim_poly_g;
+
+    ## Default(s)
+
+    my $tmpl = {
+        detect_pe_adapter => {
+            allow       => [ 0, 1 ],
+            store       => \$detect_pe_adapter,
+            strict_type => 1,
+        },
+        filehandle => {
+            store => \$filehandle,
+        },
+        first_infile_path => {
+            defined     => 1,
+            required    => 1,
+            store       => \$first_infile_path,
+            strict_type => 1,
+        },
+        first_outfile_path => {
+            defined     => 1,
+            required    => 1,
+            store       => \$first_outfile_path,
+            strict_type => 1,
+        },
+        interleaved_in => {
+            allow       => [ 0, 1 ],
+            store       => \$interleaved_in,
+            strict_type => 1,
+        },
+        length_required => {
+            allow       => qr/\A \d+ \z/xms,
+            store       => \$length_required,
+            strict_type => 1,
+        },
+        low_complexity_filter => {
+            allow       => [ 0, 1 ],
+            store       => \$low_complexity_filter,
+            strict_type => 1,
+        },
+        overrepresentation_analysis => {
+            allow       => [ 0, 1 ],
+            store       => \$overrepresentation_analysis,
+            strict_type => 1,
+        },
+        second_infile_path => {
+            store       => \$second_infile_path,
+            strict_type => 1,
+        },
+        second_outfile_path => {
+            store       => \$second_outfile_path,
+            strict_type => 1,
+        },
+        stderrfile_path => {
+            store       => \$stderrfile_path,
+            strict_type => 1,
+        },
+        stderrfile_path_append => {
+            store       => \$stderrfile_path_append,
+            strict_type => 1,
+        },
+        stdoutfile_path => {
+            store       => \$stdoutfile_path,
+            strict_type => 1,
+        },
+        threads => {
+            allow       => qr/\A \d+ \z/xms,
+            defined     => 1,
+            store       => \$threads,
+            strict_type => 1,
+          },
+        trim_poly_g => {
+            allow       => [ 0, 1 ],
+            store       => \$trim_poly_g,
+            strict_type => 1,
+        },
+    };
+
+    check( $tmpl, $arg_href, 1 ) or croak q{Could not parse arguments!};
+
+    my @commands = ( get_executable_base_command( { base_command => $BASE_COMMAND, } ), );
+
+    push @commands, q{--in1} . $SPACE . $first_infile_path;
+
+    push @commands, q{--out1} . $SPACE . $first_outfile_path;
+
+    if ($detect_pe_adapter) {
+
+        push @commands, q{--detect_adapter_for_pe};
+    }
+
+    if ($interleaved_in) {
+
+        push @commands, q{--interleaved_in};
+    }
+
+    if ($length_required) {
+
+        push @commands, q{--length_required} . $SPACE . $length_required;
+    }
+
+    if ($low_complexity_filter) {
+
+        push @commands, q{--low_complexity_filter};
+    }
+
+    if ($overrepresentation_analysis) {
+
+        push @commands, q{--overrepresentation_analysis};
+    }
+
+    if ($second_infile_path) {
+
+        push @commands, q{--in2} . $SPACE . $second_infile_path;
+    }
+
+    if ($second_outfile_path) {
+
+        push @commands, q{--out2} . $SPACE . $second_outfile_path;
+    }
+
+    if ($threads) {
+
+        push @commands, q{--threads} . $SPACE . $threads;
+    }
+
+    if ($trim_poly_g) {
+
+        push @commands, q{--trim_poly_g};
+    }
+
+    push @commands,
+      unix_standard_streams(
+        {
+            stderrfile_path        => $stderrfile_path,
+            stderrfile_path_append => $stderrfile_path_append,
+            stdoutfile_path        => $stdoutfile_path,
+        }
+      );
+
+    unix_write_to_file(
+        {
+            commands_ref => \@commands,
+            filehandle   => $filehandle,
+            separator    => $SPACE,
+
+        }
+    );
+    return @commands;
+}
+
+1;
+
---
+++ t/fastp.t
@@ -0,0 +1,151 @@
+#!/usr/bin/env perl
+
+use 5.026;
+use Carp;
+use charnames qw{ :full :short };
+use English qw{ -no_match_vars };
+use File::Basename qw{ dirname };
+use File::Spec::Functions qw{ catdir };
+use FindBin qw{ $Bin };
+use open qw{ :encoding(UTF-8) :std };
+use Params::Check qw{ allow check last_error };
+use Test::More;
+use utf8;
+use warnings qw{ FATAL utf8 };
+
+## CPANM
+use autodie qw{ :all };
+use Modern::Perl qw{ 2018 };
+use Readonly;
+
+## MIPs lib/
+use lib catdir( dirname($Bin), q{lib} );
+use MIP::Constants qw{ $COMMA $SPACE };
+use MIP::Test::Commands qw{ test_function };
+
+BEGIN {
+
+    use MIP::Test::Fixtures qw{ test_import };
+
+### Check all internal dependency modules and imports
+## Modules with import
+    my %perl_module = ( q{MIP::Program::Fastp} => [qw{ fastp }], );
+
+    test_import( { perl_module_href => \%perl_module, } );
+}
+
+## Constants
+Readonly my $REQUIRED_LENGTH => 50;
+
+use MIP::Program::Fastp qw{ fastp };
+
+diag(   q{Test fastp from Fastp.pm}
+      . $COMMA
+      . $SPACE . q{Perl}
+      . $SPACE
+      . $PERL_VERSION
+      . $SPACE
+      . $EXECUTABLE_NAME );
+
+## Base arguments
+my @function_base_commands = qw{fastp};
+
+my %base_argument = (
+    filehandle => {
+        input           => undef,
+        expected_output => \@function_base_commands,
+    },
+    stderrfile_path => {
+        input           => q{stderrfile.test},
+        expected_output => q{2> stderrfile.test},
+    },
+    stderrfile_path_append => {
+        input           => q{stderrfile.test},
+        expected_output => q{2>> stderrfile.test},
+    },
+    stdoutfile_path => {
+        input           => q{stdoutfile.test},
+        expected_output => q{1> stdoutfile.test},
+    },
+);
+
+## Can be duplicated with %base_argument and/or %specific_argument
+## to enable testing of each individual argument
+my %required_argument = (
+    first_infile_path => {
+        input           => q{read_1.fq.gz},
+        expected_output => q{--in1 read_1.fq.gz},
+    },
+    first_outfile_path => {
+        input           => q{read_trim_1.fq.gz},
+        expected_output => q{--out1 read_trim_1.fq.gz},
+    },
+);
+
+my %specific_argument = (
+    detect_pe_adapter => {
+        input           => 1,
+        expected_output => q{--detect_adapter_for_pe}
+    },
+    first_infile_path => {
+        input           => q{read_1.fq.gz},
+        expected_output => q{--in1 read_1.fq.gz},
+    },
+    first_outfile_path => {
+        input           => q{read_trim_1.fq.gz},
+        expected_output => q{--out1 read_trim_1.fq.gz},
+    },
+    interleaved_in => {
+        input => 1,
+        expected_output => q{--interleaved_in},
+    },
+    length_required => {
+        input           => $REQUIRED_LENGTH,
+        expected_output => q{--length_required} . $SPACE . $REQUIRED_LENGTH,
+    },
+    low_complexity_filter => {
+        input           => 1,
+        expected_output => q{--low_complexity_filter},
+    },
+    overrepresentation_analysis => {
+        input           => 1,
+        expected_output => q{--overrepresentation_analysis},
+    },
+    second_infile_path => {
+        input           => q{read_2.fq.gz},
+        expected_output => q{--in2 read_2.fq.gz},
+    },
+    second_outfile_path => {
+        input           => q{read_trim_2.fq.gz},
+        expected_output => q{--out2 read_trim_2.fq.gz},
+    },
+    threads => {
+        input => 4,
+        expected_output => q{--threads 4}
+    },
+    trim_poly_g => {
+        input           => 1,
+        expected_output => q{--trim_poly_g},
+    },
+);
+
+## Coderef - enables generalized use of generate call
+my $module_function_cref = \&fastp;
+
+## Test both base and function specific arguments
+my @arguments = ( \%base_argument, \%specific_argument );
+
+ARGUMENT_HASH_REF:
+foreach my $argument_href (@arguments) {
+    my @commands = test_function(
+        {
+            argument_href              => $argument_href,
+            do_test_base_command       => 1,
+            function_base_commands_ref => \@function_base_commands,
+            module_function_cref       => $module_function_cref,
+            required_argument_href     => \%required_argument,
+        }
+    );
+}
+
+done_testing();
